2d6585b57fa072ac3655ae7e095d2550de5a1f0e
pankoivan <panivan98@yandex.ru>
2023-12-07 17:31:04 +0300

    Добавил новые таблицы, поправил старые

--- sql/Creating All Tables.sql
+++ sql/Creating All Tables.sql
@@ -1,30 +1,47 @@
-/* Контакты на главной странице */
+/* Контакты главного администратора */
 
-CREATE TABLE main_page_contact (
+CREATE TABLE main_admin_contact (
 	id SERIAL PRIMARY KEY,
 	contact VARCHAR (256) UNIQUE NOT NULL,
 	description VARCHAR (128) UNIQUE NOT NULL
 );
 
-/* Коды для регистрации */
+/* Зарегистрированные медицинские учреждения */
+
+CREATE TABLE registered_hospital (
+	id SERIAL PRIMARY KEY,
+	oid VARCHAR (512) UNIQUE NOT NULL,
+	name VARCHAR (1024) UNIQUE NOT NULL,
+	date TIMESTAMP NOT NULL
+);
+
+/* Коды безопасности для регистрации администраторов, врачей и пациентов */
 
 CREATE TABLE registration_security_code (
 	id SERIAL PRIMARY KEY,
+	registered_hospital_id INT REFERENCES registered_hospital (id) NOT NULL,
 	role VARCHAR (32) NOT NULL,
-	hospital_id VARCHAR (512) NOT NULL,
 	code UUID UNIQUE NOT NULL,
 	expiration_date DATE NOT NULL
 );
 
-/* Коды для регистрации */
+/* Коды безопасности для доступа к странице (форме) регистрации медицинского учреждения */
+
+CREATE TABLE page_access_security_code (
+	id SERIAL PRIMARY KEY,
+	code UUID UNIQUE NOT NULL,
+	expiration_date DATE NOT NULL
+);
+
+/* Почты и пароли всех пользователей */
 
 CREATE TABLE user_security_information (
 	id SERIAL PRIMARY KEY,
-	email VARCHAR (80) UNIQUE NOT NULL,
+	email VARCHAR (256) UNIQUE NOT NULL,
 	password VARCHAR (256) NOT NULL
 );
 
-/* Коды для регистрации */
+/* Общая информация обо всех пользователях */
 
 CREATE TABLE user_information (
 	id SERIAL PRIMARY KEY,
@@ -36,15 +53,17 @@ CREATE TABLE user_information (
 	phone_number VARCHAR (24)
 );
 
-/* Коды для регистрации */
+/* Информация о сотрудниках медицинского учреждения */
 
 CREATE TABLE hospital_employee_information (
 	id SERIAL PRIMARY KEY,
 	user_information_id INT REFERENCES user_information (id) UNIQUE NOT NULL,
+	registered_hospital_id INT REFERENCES registered_hospital (id) NOT NULL,
+	post VARCHAR (128) NOT NULL,
 	experience INT
 );
 
-/* Коды для регистрации */
+/* Главный администратор */
 
 CREATE TABLE main_administrator (
 	id SERIAL PRIMARY KEY,
@@ -52,67 +71,97 @@ CREATE TABLE main_administrator (
 	user_information_id INT REFERENCES user_information (id) UNIQUE NOT NULL
 );
 
-/* Коды для регистрации */
+/* Администраторы */
 
 CREATE TABLE administrator (
 	id SERIAL PRIMARY KEY,
 	user_security_information_id INT REFERENCES user_security_information (id) UNIQUE NOT NULL,
-	hospital_employee_information_id INT REFERENCES hospital_employee_information (id) UNIQUE NOT NULL,
-	hospital_id VARCHAR (512) NOT NULL
+	hospital_employee_information_id INT REFERENCES hospital_employee_information (id) UNIQUE NOT NULL
 );
 
-/* Коды для регистрации */
+/* Врачи */
 
 CREATE TABLE doctor (
 	id SERIAL PRIMARY KEY,
 	user_security_information_id INT REFERENCES user_security_information (id) UNIQUE NOT NULL,
 	hospital_employee_information_id INT REFERENCES hospital_employee_information (id) UNIQUE NOT NULL,
-	hospital_id VARCHAR (512) NOT NULL,
 	university VARCHAR (256)
 );
 
-/* Коды для регистрации */
+/* Пациенты */
 
 CREATE TABLE patient (
 	id SERIAL PRIMARY KEY,
 	user_security_information_id INT REFERENCES user_security_information (id) UNIQUE NOT NULL,
 	user_information_id INT REFERENCES user_information (id) UNIQUE NOT NULL,
 	doctor_id INT REFERENCES doctor (id),
-	hospital_id VARCHAR (512) NOT NULL
+	registered_hospital_id INT REFERENCES registered_hospital (id) NOT NULL,
 );
 
-/* Коды для регистрации */
+/* Наблюдения */
 
-CREATE TABLE patient_examination (
+CREATE TABLE examination (
 	id SERIAL PRIMARY KEY,
 	patient_id INT REFERENCES patient (id) NOT NULL,
 	doctor_id INT REFERENCES doctor (id) NOT NULL,
-	examination_date DATE NOT NULL,
-	breathlessness VARCHAR (1024),
-	fatigue VARCHAR (1024),
-	weakness VARCHAR (1024),
-	vertigo VARCHAR (1024),
-	edema VARCHAR (1024),
-	hepatomegaly VARCHAR (1024),
-	ascites VARCHAR (1024),
-	pulsation VARCHAR (1024),
-	chest_pain VARCHAR (1024),
-	hemoptysis VARCHAR (1024),
-	pulse_oximetry VARCHAR (1024),
-	angina VARCHAR (1024),
-	cough VARCHAR (1024),
-	changes_in_fingers VARCHAR (1024),
-	appetite VARCHAR (1024),
-	blood_test_link VARCHAR (1024),
-	electrocardiography_link VARCHAR (1024),
-	radiography_link VARCHAR (1024),
-	echocardiography_link VARCHAR (1024),
-	chest_tomography_link VARCHAR (1024)
+	date TIMESTAMP NOT NULL
 );
 
-/* Коды для регистрации */
+/* Показатели */
 
-CREATE TABLE hospital_employee_sick_leave (
+CREATE TABLE indicator (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (128) UNIQUE NOT NULL,
+    group VARCHAR (32) NOT NULL
+);
+
+/* Конкретные показатели в конкретном наблюдении */
+
+CREATE TABLE examination_indicator (
+    id SERIAL PRIMARY KEY,
+    examination_id INT REFERENCES examination (id) NOT NULL,
+    indicator_id INT REFERENCES indicator (id) NOT NULL,
+    value VARCHAR (256) NOT NULL
+);
+
+/* Расписания сбора показателей */
+
+CREATE TABLE indicator_schedule (
+    id SERIAL PRIMARY KEY,
+    patient_id INT REFERENCES patient (id) NOT NULL,
+    indicator_id INT REFERENCES indicator (id) NOT NULL,
+    schedule VARCHAR (64) NOT NULL
+);
+
+/* Лекарства, назначенные пациентам */
+
+CREATE TABLE medicine (
+    id SERIAL PRIMARY KEY,
+    patient_id INT REFERENCES patient (id) NOT NULL,
+    medicine_api_id VARCHAR (1024) NOT NULL,
+    description TEXT NOT NULL
+);
+
+/* Награды пациентов */
+
+CREATE TABLE achievement (
+    id SERIAL PRIMARY KEY,
+    filename VARCHAR (256) UNIQUE NOT NULL,
+    name VARCHAR (64) UNIQUE NOT NULL,
+    description TEXT UNIQUE NOT NULL
+);
+
+/* Связи пациентов и наград */
+
+CREATE TABLE patient_achievement (
+    id SERIAL PRIMARY KEY,
+    patient_id INT REFERENCES patient (id) NOT NULL,
+    achievement_id INT REFERENCES achievement (id) NOT NULL
+);
+
+/* Больничные сотрудников медицинских учреждений */
+
+CREATE TABLE sick_leave (
 	id SERIAL PRIMARY KEY,
 	hospital_employee_information_id INT REFERENCES hospital_employee_information (id) NOT NULL,
 	author_id INT REFERENCES administrator (id) NOT NULL,
@@ -121,9 +170,9 @@ CREATE TABLE hospital_employee_sick_leave (
 	comment TEXT
 );
 
-/* Коды для регистрации */
+/* Отпуски сотрудников медицинских учреждений */
 
-CREATE TABLE hospital_employee_vacation (
+CREATE TABLE vacation (
 	id SERIAL PRIMARY KEY,
 	hospital_employee_information_id INT REFERENCES hospital_employee_information (id) NOT NULL,
 	author_id INT REFERENCES administrator (id) NOT NULL,
@@ -132,25 +181,27 @@ CREATE TABLE hospital_employee_vacation (
 	comment TEXT
 );
 
-/* Коды для регистрации */
+/* Увольнения сотрудников медицинских учреждений */
 
-CREATE TABLE hospital_employee_dismissal (
+CREATE TABLE dismissal (
 	id SERIAL PRIMARY KEY,
 	hospital_employee_information_id INT REFERENCES hospital_employee_information (id) UNIQUE NOT NULL,
 	author_id INT REFERENCES administrator (id) NOT NULL,
+	date TIME NOT NULL,
 	comment TEXT
 );
 
-/* Коды для регистрации */
+/* Выписки пациентов */
 
-CREATE TABLE patient_recovery (
+CREATE TABLE recovery (
 	id SERIAL PRIMARY KEY,
 	patient_id INT REFERENCES patient (id) UNIQUE NOT NULL,
 	author_id INT REFERENCES doctor (id) NOT NULL,
+	date TIME NOT NULL,
 	comment TEXT
 );
 
-/* Коды для регистрации */
+/* Сообщения всех пользователей */
 
 CREATE TABLE user_message (
 	id SERIAL PRIMARY KEY,
