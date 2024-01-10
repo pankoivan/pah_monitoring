8d9a113c00d26b27e9339d87ebf69fc8590f3782
pankoivan <panivan98@yandex.ru>
2024-01-10 13:02:25 +0300

    Наполнил сервисы по работе с сущностью MainAdminContact.

--- src/main/java/org/pah_monitoring/main/services/other/implementations/MainAdminContactServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/other/implementations/MainAdminContactServiceImpl.java
@@ -1,14 +1,32 @@
 package org.pah_monitoring.main.services.other.implementations;
 
 import lombok.AllArgsConstructor;
+import org.pah_monitoring.main.entities.other.MainAdminContact;
 import org.pah_monitoring.main.repositorites.other.MainAdminContactRepository;
 import org.pah_monitoring.main.services.other.interfaces.MainAdminContactService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class MainAdminContactServiceImpl implements MainAdminContactService {
 
     private final MainAdminContactRepository repository;
+
+    @Override
+    public List<MainAdminContact> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public MainAdminContact save(MainAdminContact contact) {
+        return repository.save(contact);
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+        repository.deleteById(id);
+    }
 
 }
--- src/main/java/org/pah_monitoring/main/services/other/interfaces/MainAdminContactService.java
+++ src/main/java/org/pah_monitoring/main/services/other/interfaces/MainAdminContactService.java
@@ -1,5 +1,15 @@
 package org.pah_monitoring.main.services.other.interfaces;
 
+import org.pah_monitoring.main.entities.other.MainAdminContact;
+
+import java.util.List;
+
 public interface MainAdminContactService {
+
+    List<MainAdminContact> findAll();
+
+    MainAdminContact save(MainAdminContact contact);
+
+    void deleteById(Integer id);
 
 }
