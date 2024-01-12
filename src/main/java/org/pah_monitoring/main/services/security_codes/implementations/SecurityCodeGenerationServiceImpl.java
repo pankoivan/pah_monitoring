a12d048ad757c30bffd6a33ec259410ab561deb8
pankoivan <panivan98@yandex.ru>
2024-01-12 17:59:01 +0300

    В соответствии с новой задумкой удалил неиспользуемые js-код, html-страницу, сервис, контроллер, репозиторий и сущность PageAccessSecurityCode. Переименовал js-файл по работе с контактами.

--- src/main/java/org/pah_monitoring/main/services/security_codes/implementations/SecurityCodeGenerationServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/security_codes/implementations/SecurityCodeGenerationServiceImpl.java
@@ -1,7 +1,6 @@
 package org.pah_monitoring.main.services.security_codes.implementations;
 
 import lombok.AllArgsConstructor;
-import org.pah_monitoring.main.services.security_codes.interfaces.PageAccessSecurityCodeService;
 import org.pah_monitoring.main.services.security_codes.interfaces.RegistrationSecurityCodeService;
 import org.pah_monitoring.main.services.security_codes.interfaces.SecurityCodeGenerationService;
 import org.springframework.stereotype.Service;
@@ -14,12 +13,10 @@ public class SecurityCodeGenerationServiceImpl implements SecurityCodeGeneration
 
     private final RegistrationSecurityCodeService registrationSecurityCodeService;
 
-    private final PageAccessSecurityCodeService pageAccessSecurityCodeService;
-
     @Override
     public UUID generate() {
         UUID code = UUID.randomUUID();
-        while (registrationSecurityCodeService.existsByCode(code) || pageAccessSecurityCodeService.existsByCode(code)) {
+        while (registrationSecurityCodeService.existsByCode(code)) {
             code = UUID.randomUUID();
         }
         return code;
