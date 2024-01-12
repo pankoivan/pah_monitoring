59d102e2a031e729dbedc295f224e54f00b6e98b
pankoivan <panivan98@yandex.ru>
2024-01-12 20:13:14 +0300

    Удалил неиспользуемый DTO-класс.

--- src/main/java/org/pah_monitoring/main/entities/dto/PageAccessSecurityCodeSavingDto.java
+++
@@ -1,7 +0,0 @@
-package org.pah_monitoring.main.entities.dto;
-
-import org.pah_monitoring.main.entities.enums.ExpirationDate;
-
-public record PageAccessSecurityCodeSavingDto(ExpirationDate expirationDate) {
-
-}
