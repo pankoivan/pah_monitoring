1781cf04b31b25c2aa38ffe03f29a6f73e7eebf9
pankoivan <panivan98@yandex.ru>
2024-01-13 13:34:59 +0300

    Добавил MVC-контроллер для выдачи страницы регистрации больницы.

---
+++ src/main/java/org/pah_monitoring/main/controllers/mvc/HospitalRegistrationRequestController.java
@@ -0,0 +1,18 @@
+package org.pah_monitoring.main.controllers.mvc;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hospital-registration-requests")
+@PreAuthorize("permitAll()")
+public class HospitalRegistrationRequestController {
+
+    @GetMapping
+    public String getPage() {
+        return "hospital-registration";
+    }
+
+}
