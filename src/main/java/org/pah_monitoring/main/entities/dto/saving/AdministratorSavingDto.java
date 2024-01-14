8a88c738d0d55ac923eef939a5903e6eca6e4ff6
pankoivan <panivan98@yandex.ru>
2024-01-14 14:05:12 +0300

    Полностью реализовал сервис регистрации администратора с отловом всех возможных ошибок.

--- src/main/java/org/pah_monitoring/main/entities/dto/saving/AdministratorSavingDto.java
+++ src/main/java/org/pah_monitoring/main/entities/dto/saving/AdministratorSavingDto.java
@@ -17,4 +17,6 @@ public class AdministratorSavingDto {
 
     private EmployeeInformationSavingDto employeeInformationSavingDto;
 
+    private String code;
+
 }
--- src/main/java/org/pah_monitoring/main/services/users/users/implementations/AdministratorServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/users/users/implementations/AdministratorServiceImpl.java
@@ -1,15 +1,19 @@
 package org.pah_monitoring.main.services.users.users.implementations;
 
 import lombok.AllArgsConstructor;
+import org.pah_monitoring.auxiliary.constants.DateTimeFormatConstants;
 import org.pah_monitoring.main.entities.dto.saving.AdministratorSavingDto;
 import org.pah_monitoring.main.entities.dto.saving.EmployeeInformationSavingDto;
 import org.pah_monitoring.main.entities.dto.saving.UserSecurityInformationSavingDto;
+import org.pah_monitoring.main.entities.enums.Role;
 import org.pah_monitoring.main.entities.security_codes.RegistrationSecurityCode;
 import org.pah_monitoring.main.entities.users.Administrator;
 import org.pah_monitoring.main.exceptions.service.DataSavingServiceException;
 import org.pah_monitoring.main.exceptions.service.DataSearchingServiceException;
 import org.pah_monitoring.main.exceptions.service.DataValidationServiceException;
+import org.pah_monitoring.main.exceptions.utils.UuidUtilsException;
 import org.pah_monitoring.main.repositorites.users.AdministratorRepository;
+import org.pah_monitoring.main.services.security_codes.interfaces.RegistrationSecurityCodeService;
 import org.pah_monitoring.main.services.users.info.interfaces.EmployeeInformationService;
 import org.pah_monitoring.main.services.users.info.interfaces.UserSecurityInformationService;
 import org.pah_monitoring.main.services.users.users.interfaces.AdministratorService;
@@ -22,9 +26,11 @@ public class AdministratorServiceImpl implements AdministratorService {
 
     private final AdministratorRepository repository;
 
-    private final UserSecurityInformationService userSecurityInformationService;
+    private final UserSecurityInformationService securityInformationService;
 
     private final EmployeeInformationService employeeInformationService;
+
+    private final RegistrationSecurityCodeService codeService;
 
     @Override
     public Administrator findById(Integer id) throws DataSearchingServiceException {
@@ -52,18 +58,29 @@ public class AdministratorServiceImpl implements AdministratorService {
             throw new DataValidationServiceException(bindingResultAnyErrorMessage(bindingResult));
         }
 
-        userSecurityInformationService.checkDataValidityForSaving(administratorSavingDto.getUserSecurityInformationSavingDto(), bindingResult);
+        securityInformationService.checkDataValidityForSaving(administratorSavingDto.getUserSecurityInformationSavingDto(), bindingResult);
         employeeInformationService.checkDataValidityForSaving(administratorSavingDto.getEmployeeInformationSavingDto(), bindingResult);
 
     }
 
-    private Administrator create(AdministratorSavingDto savingDto) throws DataSavingServiceException  {
-
-        if (codeDoesNotExists || isCodeExpired || isCodeNotForRole) {
-            throw new DataValidationServiceException("");
-        }
+    private Administrator create(AdministratorSavingDto savingDto) throws DataSavingServiceException {
 
         RegistrationSecurityCode code;
+        try {
+            code = codeService.findByStringUuid(savingDto.getCode());
+        } catch (UuidUtilsException | DataSearchingServiceException e) {
+            throw new DataSavingServiceException(e.getMessage(), e);
+        }
+
+        if (codeService.isExpired(code)) {
+            throw new DataSavingServiceException(
+                    "Истёк срок действия кода. Код был действителен до %s"
+                            .formatted(DateTimeFormatConstants.DAY_MONTH_YEAR_AT_HOUR_MINUTE_SECOND.format(code.getExpirationDate()))
+            );
+        }
+        if (!codeService.isSuitableForRole(code, Role.ADMINISTRATOR)) {
+            throw new DataSavingServiceException("Код не предназначен для роли \"%s\"".formatted(Role.ADMINISTRATOR));
+        }
 
         UserSecurityInformationSavingDto securityInformationSavingDto = savingDto.getUserSecurityInformationSavingDto();
         securityInformationSavingDto.setId(null);
@@ -77,7 +94,7 @@ public class AdministratorServiceImpl implements AdministratorService {
             return repository.save(
                     Administrator
                             .builder()
-                            .userSecurityInformation(userSecurityInformationService.save(securityInformationSavingDto))
+                            .userSecurityInformation(securityInformationService.save(securityInformationSavingDto))
                             .employeeInformation(employeeInformationService.save(employeeInformationSavingDto, code.getHospital()))
                             .build()
             );
@@ -101,7 +118,7 @@ public class AdministratorServiceImpl implements AdministratorService {
             return repository.save(
                     Administrator
                             .builder()
-                            .userSecurityInformation(userSecurityInformationService.save(securityInformationSavingDto))
+                            .userSecurityInformation(securityInformationService.save(securityInformationSavingDto))
                             .employeeInformation(employeeInformationService.save(
                                     employeeInformationSavingDto,
                                     administrator.getEmployeeInformation().getHospital())
@@ -111,10 +128,6 @@ public class AdministratorServiceImpl implements AdministratorService {
         } catch (Exception e) {
             throw new DataSavingServiceException("DTO-сущность \"%s\" не была сохранена".formatted(savingDto), e);
         }
-
-    }
-
-    private RegistrationSecurityCode checkCodeValidity(String stringUuid) throws DataValidationServiceException {
 
     }
 
