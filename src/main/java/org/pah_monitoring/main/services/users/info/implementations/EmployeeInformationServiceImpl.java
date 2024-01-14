3fe12ba2aaaddf86b5c9b2c3e0d551e3e4543606
pankoivan <panivan98@yandex.ru>
2024-01-14 12:31:25 +0300

    Поправил сервис EmployeeInformationService, добавил в метод save() больницу для связи.

--- src/main/java/org/pah_monitoring/main/services/users/info/implementations/EmployeeInformationServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/users/info/implementations/EmployeeInformationServiceImpl.java
@@ -2,11 +2,13 @@ package org.pah_monitoring.main.services.users.info.implementations;
 
 import lombok.AllArgsConstructor;
 import org.pah_monitoring.main.entities.dto.saving.EmployeeInformationSavingDto;
+import org.pah_monitoring.main.entities.hospitals.Hospital;
 import org.pah_monitoring.main.entities.users.info.EmployeeInformation;
 import org.pah_monitoring.main.exceptions.service.DataSavingServiceException;
 import org.pah_monitoring.main.exceptions.service.DataValidationServiceException;
 import org.pah_monitoring.main.repositorites.users.info.EmployeeInformationRepository;
 import org.pah_monitoring.main.services.users.info.interfaces.EmployeeInformationService;
+import org.pah_monitoring.main.services.users.info.interfaces.UserInformationService;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 
@@ -16,22 +18,30 @@ public class EmployeeInformationServiceImpl implements EmployeeInformationServic
 
     private final EmployeeInformationRepository repository;
 
+    private final UserInformationService userInformationService;
+
     @Override
-    public EmployeeInformation save(EmployeeInformationSavingDto employeeInformationDto) throws DataSavingServiceException {
+    public EmployeeInformation save(EmployeeInformationSavingDto employeeInformationDto, Hospital hospital)
+            throws DataSavingServiceException {
+
         try {
-            return null; // todo
-
-            if (isNew()) {
-
-            }
-
+            return repository.save(
+                    EmployeeInformation
+                            .builder()
+                            .id(employeeInformationDto.getId())
+                            .post(employeeInformationDto.getPost())
+                            .userInformation(userInformationService.save(employeeInformationDto.getUserInformationSavingDto()))
+                            .hospital(hospital)
+                            .build()
+            );
         } catch (Exception e) {
-            throw new DataSavingServiceException("DTO-сущность \"%s\" не была сохранена".formatted(employeeInformationDto));
+            throw new DataSavingServiceException("DTO-сущность \"%s\" не была сохранена".formatted(employeeInformationDto), e);
         }
+
     }
 
     @Override
-    public void checkDataValidityForSaving(EmployeeInformation employeeInformation, BindingResult bindingResult)
+    public void checkDataValidityForSaving(EmployeeInformationSavingDto employeeInformation, BindingResult bindingResult)
             throws DataValidationServiceException {
 
         if (bindingResult.hasErrors()) {
--- src/main/java/org/pah_monitoring/main/services/users/info/interfaces/EmployeeInformationService.java
+++ src/main/java/org/pah_monitoring/main/services/users/info/interfaces/EmployeeInformationService.java
@@ -1,12 +1,13 @@
 package org.pah_monitoring.main.services.users.info.interfaces;
 
 import org.pah_monitoring.main.entities.dto.saving.EmployeeInformationSavingDto;
+import org.pah_monitoring.main.entities.hospitals.Hospital;
 import org.pah_monitoring.main.entities.users.info.EmployeeInformation;
 import org.pah_monitoring.main.exceptions.service.DataSavingServiceException;
 import org.pah_monitoring.main.services.validation.interfaces.SavingValidationService;
 
-public interface EmployeeInformationService extends SavingValidationService<EmployeeInformation> {
+public interface EmployeeInformationService extends SavingValidationService<EmployeeInformationSavingDto> {
 
-    EmployeeInformation save(EmployeeInformationSavingDto employeeInformationDto) throws DataSavingServiceException;
+    EmployeeInformation save(EmployeeInformationSavingDto employeeInformationDto, Hospital hospital) throws DataSavingServiceException;
 
 }
