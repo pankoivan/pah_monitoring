0b163fc3955a054c8ca8ad3ccca4497e18455697
pankoivan <panivan98@yandex.ru>
2024-01-16 10:56:07 +0300

    Добавил редиректы в главный контроллер, чтобы информационная main-страница была видна только неаутентифицированным пользователям, а для остальных выдавались конкретные страницы.

--- src/main/java/org/pah_monitoring/main/controllers/mvc/MainMvcController.java
+++ src/main/java/org/pah_monitoring/main/controllers/mvc/MainMvcController.java
@@ -1,8 +1,13 @@
 package org.pah_monitoring.main.controllers.mvc;
 
 import lombok.AllArgsConstructor;
+import org.pah_monitoring.main.entities.users.Administrator;
+import org.pah_monitoring.main.entities.users.Doctor;
+import org.pah_monitoring.main.entities.users.MainAdministrator;
+import org.pah_monitoring.main.entities.users.Patient;
 import org.pah_monitoring.main.services.main_admin_contacts.interfaces.MainAdminContactService;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,9 +22,27 @@ public class MainMvcController {
     private final MainAdminContactService mainAdminContactService;
 
     @GetMapping
-    public String getPage(Model model) {
-        model.addAttribute("contacts", mainAdminContactService.findAll());
-        return "main";
+    public String getPage(Model model, @AuthenticationPrincipal Object principal) {
+
+        return switch (principal) {
+
+            case MainAdministrator ignored -> "redirect:/hospitals";
+
+            case Administrator administrator -> "redirect:/hospitals/%s"
+                    .formatted(administrator.getEmployeeInformation().getHospital().getId());
+
+            case Doctor doctor -> "redirect:/hospitals/%s/doctors/%s/patients"
+                    .formatted(doctor.getEmployeeInformation().getHospital().getId(), doctor.getId());
+
+            case Patient ignored -> "redirect:/indicators";
+
+            default -> {
+                model.addAttribute("contacts", mainAdminContactService.findAll());
+                yield "main";
+
+            }
+        };
+
     }
 
 }
