50e4900879b282d4bb7023edba2177160a3d042b
pankoivan <panivan98@yandex.ru>
2024-01-20 22:27:32 +0300

    Добавил расширение интерфейсов проверки данных при добавлении и редактировании для интерфейса UserInfoService.

--- src/main/java/org/pah_monitoring/main/services/users/info/interfaces/common/UserInfoService.java
+++ src/main/java/org/pah_monitoring/main/services/users/info/interfaces/common/UserInfoService.java
@@ -3,22 +3,19 @@ package org.pah_monitoring.main.services.users.info.interfaces.common;
 import org.pah_monitoring.main.entities.users.users.common.User;
 import org.pah_monitoring.main.exceptions.service.DataSavingServiceException;
 import org.pah_monitoring.main.exceptions.service.DataSearchingServiceException;
-import org.pah_monitoring.main.exceptions.service.DataValidationServiceException;
 import org.pah_monitoring.main.exceptions.service.NotEnoughRightsServiceException;
+import org.pah_monitoring.main.services.validation.interfaces.data.saving.DataAddingValidationService;
+import org.pah_monitoring.main.services.validation.interfaces.data.saving.DataEditingValidationService;
 import org.pah_monitoring.main.services.validation.interfaces.data.saving.DataSavingValidationService;
-import org.springframework.validation.BindingResult;
 
-public interface UserInfoService<T, M, R, N> extends DataSavingValidationService<N> {
+public interface UserInfoService<T, M, R, N>
+        extends DataAddingValidationService<M>, DataEditingValidationService<R>, DataSavingValidationService<N> {
 
     T findById(Integer id) throws DataSearchingServiceException;
 
     T add(M addingDto) throws DataSavingServiceException;
 
     T edit(R editingDto) throws DataSavingServiceException;
-
-    void checkDataValidityForAdding(M addingDto, BindingResult bindingResult) throws DataValidationServiceException;
-
-    void checkDataValidityForEditing(R editingDto, BindingResult bindingResult) throws DataValidationServiceException;
 
     void checkAccessRightsForEditing(User userWithRequestedEditingInfo) throws NotEnoughRightsServiceException;
 
