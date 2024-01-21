f0ad61654b676e30ef5d1292574ee224fceb7cd3
pankoivan <panivan98@yandex.ru>
2024-01-21 10:42:06 +0300

    Добавил иерархию DTO для сообщений пользователей.

---
+++ src/main/java/org/pah_monitoring/main/entities/dto/saving/users/messages/UserMessageAddingDto.java
@@ -0,0 +1,12 @@
+package org.pah_monitoring.main.entities.dto.saving.users.messages;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class UserMessageAddingDto extends UserMessageSavingDto {
+
+    Integer recipientId;
+
+}
