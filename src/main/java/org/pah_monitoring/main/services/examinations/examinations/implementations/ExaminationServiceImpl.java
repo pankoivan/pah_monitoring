46b7bef97c5a7d7bb87a111fe7b968e861936e8c
pankoivan <panivan98@yandex.ru>
2024-01-22 11:21:51 +0300

    Реализовал сервис для создания наблюдения.

--- src/main/java/org/pah_monitoring/main/services/examinations/examinations/implementations/ExaminationServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/examinations/examinations/implementations/ExaminationServiceImpl.java
@@ -1,14 +1,42 @@
 package org.pah_monitoring.main.services.examinations.examinations.implementations;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.pah_monitoring.main.entities.examinations.examinations.Examination;
+import org.pah_monitoring.main.entities.users.users.Patient;
+import org.pah_monitoring.main.exceptions.service.data.DataSavingServiceException;
 import org.pah_monitoring.main.repositorites.examinations.examinations.ExaminationRepository;
+import org.pah_monitoring.main.services.auxiliary.access.interfaces.CurrentUserExtractionService;
 import org.pah_monitoring.main.services.examinations.examinations.interfaces.ExaminationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Setter(onMethod = @__(@Autowired))
 @Service
 public class ExaminationServiceImpl implements ExaminationService {
 
     private final ExaminationRepository repository;
+
+    private CurrentUserExtractionService extractionService;
+
+    @Override
+    public Examination create() throws DataSavingServiceException {
+        try {
+            Patient patient = extractionService.patient();
+            return repository.save(
+                    Examination
+                            .builder()
+                            .patient(patient)
+                            .doctor(patient.getDoctor())
+                            .date(LocalDateTime.now())
+                            .build()
+            );
+        } catch (Exception e) {
+            throw new DataSavingServiceException("Наблюдение не было создано", e);
+        }
+    }
 
 }
--- src/main/java/org/pah_monitoring/main/services/examinations/examinations/interfaces/ExaminationService.java
+++ src/main/java/org/pah_monitoring/main/services/examinations/examinations/interfaces/ExaminationService.java
@@ -1,5 +1,10 @@
 package org.pah_monitoring.main.services.examinations.examinations.interfaces;
 
+import org.pah_monitoring.main.entities.examinations.examinations.Examination;
+import org.pah_monitoring.main.exceptions.service.data.DataSavingServiceException;
+
 public interface ExaminationService {
+
+    Examination create() throws DataSavingServiceException;
 
 }
