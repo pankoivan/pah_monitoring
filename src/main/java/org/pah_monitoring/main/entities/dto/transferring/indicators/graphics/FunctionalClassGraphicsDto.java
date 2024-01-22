a467f3416df35fe6b299139320294938a9e34c27
pankoivan <panivan98@yandex.ru>
2024-01-22 20:58:25 +0300

    Добавил для всех показателей два вида DTO: для передачи данных для графиков и для таблиц.

---
+++ src/main/java/org/pah_monitoring/main/entities/dto/transferring/indicators/graphics/FunctionalClassGraphicsDto.java
@@ -0,0 +1,10 @@
+package org.pah_monitoring.main.entities.dto.transferring.indicators.graphics;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class FunctionalClassGraphicsDto {
+
+}
