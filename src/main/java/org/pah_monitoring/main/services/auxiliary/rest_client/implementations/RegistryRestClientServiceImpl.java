97407170522adb30fe5a30bd5abfc15838757c7a
pankoivan <panivan98@yandex.ru>
2024-01-25 11:29:25 +0300

    Добавил исключение для API-клиент-сервиса, добавил его использование в метод сервиса. Добавил исключение для будущего соответствующего REST-контроллера.

--- src/main/java/org/pah_monitoring/main/services/auxiliary/rest_client/implementations/RegistryRestClientServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/auxiliary/rest_client/implementations/RegistryRestClientServiceImpl.java
@@ -2,6 +2,7 @@ package org.pah_monitoring.main.services.auxiliary.rest_client.implementations;
 
 import lombok.*;
 import org.pah_monitoring.auxiliary.utils.UrlUtils;
+import org.pah_monitoring.main.exceptions.service.api_client.ApiClientServiceException;
 import org.pah_monitoring.main.exceptions.utils.UrlUtilsException;
 import org.pah_monitoring.main.services.auxiliary.rest_client.interfaces.RegistryRestClientService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,17 +31,24 @@ public class RegistryRestClientServiceImpl implements RegistryRestClientService 
     @Value("${my.registry-api.token}")
     private final String token;
 
-    private BaseResponse exchange(Integer page, Integer onPage) throws UrlUtilsException {
-
-        RequestEntity<Void> request = RequestEntity
-                .get(UrlUtils.buildUrlWithGetParameters(baseUrl, "identifier", identifier, "userKey", token))
-                .build();
+    private BaseResponse getBaseResponse(Integer page, Integer onPage) throws ApiClientServiceException {
 
         try {
+
+            RequestEntity<Void> request = RequestEntity
+                    .get(UrlUtils.buildUrlWithGetParameters(
+                            baseUrl,
+                            "identifier", identifier,
+                            "userKey", token,
+                            "page", page,
+                            "size", onPage
+                    ))
+                    .build();
+
             return restTemplate.exchange(request, BaseResponse.class).getBody();
-        } catch (RestClientException e) {
-            e.printStackTrace();
-            return null;
+
+        } catch (UrlUtilsException | RestClientException e) {
+            throw new ApiClientServiceException("Произошла ошибка при REST-взаимодействии с API реестра", e);
         }
 
     }
