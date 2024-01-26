ecd9cc1c8c220e43291d40b1fa060ea94f75166d
pankoivan <panivan98@yandex.ru>
2024-01-26 12:43:46 +0300

    Поправил стиль страницы логина. В логин-контроллере использовал сервис по добавлению хедера.

--- src/main/java/org/pah_monitoring/main/controllers/mvc/login/LoginMvcController.java
+++ src/main/java/org/pah_monitoring/main/controllers/mvc/login/LoginMvcController.java
@@ -1,9 +1,11 @@
 package org.pah_monitoring.main.controllers.mvc.login;
 
 import lombok.RequiredArgsConstructor;
+import org.pah_monitoring.main.services.auxiliary.mvc.interfaces.PageHeaderService;
 import org.pah_monitoring.main.services.auxiliary.mvc.interfaces.RedirectService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,11 +17,14 @@ public class LoginMvcController {
 
     private final RedirectService redirectService;
 
+    private final PageHeaderService pageHeaderService;
+
     @GetMapping
-    public String getLogin() {
+    public String getLogin(Model model) {
         if (redirectService.checkNotAnonymousUserRedirect()) {
             return redirectService.notAnonymousUserRedirect();
         }
+        pageHeaderService.addHeader(model);
         return "login";
     }
 
