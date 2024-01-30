2a7b5b127cd504069636747c7dc22094db02003e
pankoivan <panivan98@yandex.ru>
2024-01-30 18:30:38 +0300

    Добавил билдеры к сущностям-статистикам, реализовал сервис по выдаче статистики больницы.

--- src/main/java/org/pah_monitoring/main/entities/auxiliary/hospitals/PatientStatistics.java
+++ src/main/java/org/pah_monitoring/main/entities/auxiliary/hospitals/PatientStatistics.java
@@ -1,17 +1,19 @@
 package org.pah_monitoring.main.entities.auxiliary.hospitals;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Builder
 public class PatientStatistics {
 
     private int activeCount;
 
-    private int inActiveCount;
+    private int inactiveCount;
 
     private int unallocated;
 
--- src/main/java/org/pah_monitoring/main/services/auxiliary/hospitals/implementations/HospitalUsersStatisticsServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/auxiliary/hospitals/implementations/HospitalUsersStatisticsServiceImpl.java
@@ -6,9 +6,14 @@ import org.pah_monitoring.main.entities.auxiliary.hospitals.AdministratorStatist
 import org.pah_monitoring.main.entities.auxiliary.hospitals.DoctorStatistics;
 import org.pah_monitoring.main.entities.auxiliary.hospitals.PatientStatistics;
 import org.pah_monitoring.main.entities.hospitals.Hospital;
+import org.pah_monitoring.main.entities.users.users.Administrator;
+import org.pah_monitoring.main.entities.users.users.Doctor;
+import org.pah_monitoring.main.entities.users.users.Patient;
 import org.pah_monitoring.main.services.auxiliary.hospitals.interfaces.HospitalUsersStatisticsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Setter(onMethod = @__(@Autowired))
@@ -17,17 +22,62 @@ public class HospitalUsersStatisticsServiceImpl implements HospitalUsersStatisti
 
     @Override
     public AdministratorStatistics getAdministratorStatistics(Hospital hospital) {
-        return null;
+        List<Administrator> administrators = hospital.getAdministrators();
+        return AdministratorStatistics
+                .builder()
+                .activeCount(
+                        (int) administrators.stream().filter(Administrator::isActive).count()
+                )
+                .vacationCount(
+                        (int) administrators.stream().filter(Administrator::isOnVacation).count()
+                )
+                .sickLeaveCount(
+                        (int) administrators.stream().filter(Administrator::isOnSickLeave).count()
+                )
+                .dismissalCount(
+                        (int) administrators.stream().filter(Administrator::isDismissed).count()
+                )
+                .build();
     }
 
     @Override
     public DoctorStatistics getDoctorStatistics(Hospital hospital) {
-        return null;
+        List<Doctor> doctors = hospital.getDoctors();
+        return DoctorStatistics
+                .builder()
+                .activeCount(
+                        (int) doctors.stream().filter(Doctor::isActive).count()
+                )
+                .vacationCount(
+                        (int) doctors.stream().filter(Doctor::isOnVacation).count()
+                )
+                .sickLeaveCount(
+                        (int) doctors.stream().filter(Doctor::isOnSickLeave).count()
+                )
+                .dismissalCount(
+                        (int) doctors.stream().filter(Doctor::isDismissed).count()
+                )
+                .withoutPatientsCount(
+                        (int) doctors.stream().filter(Doctor::hasNoPatients).count()
+                )
+                .build();
     }
 
     @Override
     public PatientStatistics getPatientStatistics(Hospital hospital) {
-        return null;
+        List<Patient> patients = hospital.getPatients();
+        return PatientStatistics
+                .builder()
+                .activeCount(
+                        (int) patients.stream().filter(Patient::isActive).count()
+                )
+                .inactiveCount(
+                        (int) patients.stream().filter(Patient::isNotActive).count()
+                )
+                .unallocated(
+                        (int) patients.stream().filter(Patient::hasNoDoctor).count()
+                )
+                .build();
     }
 
 }
