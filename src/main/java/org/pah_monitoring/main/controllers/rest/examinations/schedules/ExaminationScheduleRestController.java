8c3c34975b7c969d833ed48f27f5475afc443ac0
pankoivan <panivan98@yandex.ru>
2024-01-31 14:12:10 +0300

    В тестовом варианте начал работу с data-атрибутами для более удобной работы с расписаниями показателей.

--- src/main/java/org/pah_monitoring/main/controllers/rest/examinations/schedules/ExaminationScheduleRestController.java
+++ src/main/java/org/pah_monitoring/main/controllers/rest/examinations/schedules/ExaminationScheduleRestController.java
@@ -9,19 +9,24 @@ import org.pah_monitoring.main.entities.enums.IndicatorType;
 import org.pah_monitoring.main.entities.examinations.schedules.ExaminationSchedule;
 import org.pah_monitoring.main.entities.users.users.Patient;
 import org.pah_monitoring.main.exceptions.controller.rest.bad_request.DataValidationRestControllerException;
+import org.pah_monitoring.main.exceptions.controller.rest.bad_request.UrlValidationRestControllerException;
 import org.pah_monitoring.main.exceptions.controller.rest.forbidden.NotEnoughRightsRestControllerException;
 import org.pah_monitoring.main.exceptions.controller.rest.internal_server.DataSavingRestControllerException;
 import org.pah_monitoring.main.exceptions.service.access.NotEnoughRightsServiceException;
 import org.pah_monitoring.main.exceptions.service.data.DataDeletionServiceException;
 import org.pah_monitoring.main.exceptions.service.data.DataSavingServiceException;
 import org.pah_monitoring.main.exceptions.service.data.DataSearchingServiceException;
+import org.pah_monitoring.main.exceptions.service.url.UrlValidationServiceException;
 import org.pah_monitoring.main.services.examinations.schedules.interfaces.ExaminationScheduleService;
 import org.pah_monitoring.main.services.users.users.interfaces.common.HospitalUserService;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 @RestController
@@ -85,6 +90,40 @@ public class ExaminationScheduleRestController {
     public static class ScheduleCheckResponse {
         private String indicatorTypeAlias;
         private String schedule;
+    }
+
+    @NoArgsConstructor @AllArgsConstructor @Data @Builder
+    public static class Temp {
+        private Integer id;
+        private Integer patientId;
+        private IndicatorType indicatorType;
+        private String indicatorTypeAlias;
+        private String schedule;
+    }
+
+    @GetMapping("/get/for/{patientId}")
+    public Map<IndicatorType, Temp> get(@PathVariable("patientId") String pathPatientId) {
+        try {
+
+            Patient patient = patientService.findById(patientService.parsePathId(pathPatientId));
+            return Stream.of(IndicatorType.values())
+                    .map(indicatorType -> {
+                                Optional<ExaminationSchedule> schedule = service.findConcrete(indicatorType, patient);
+                                return Temp
+                                        .builder()
+                                        .id(schedule.map(ExaminationSchedule::getId).orElse(null))
+                                        .patientId(patient.getId())
+                                        .indicatorType(indicatorType)
+                                        .indicatorTypeAlias(indicatorType.getAlias())
+                                        .schedule(schedule.map(ExaminationSchedule::getSchedule).orElse(null))
+                                        .build();
+                            }
+                    )
+                    .collect(Collectors.toMap(Temp::getIndicatorType, Function.identity()));
+
+        } catch (UrlValidationServiceException | DataSearchingServiceException e) {
+            throw new UrlValidationRestControllerException(e.getMessage(), e);
+        }
     }
 
 }
--- src/main/resources/static/js/patients/indicators.js
+++ src/main/resources/static/js/patients/indicators.js
@@ -1,17 +1,70 @@
-document.querySelectorAll('a[id^="edit-"]').forEach((edit) => {
-    edit.addEventListener("click", function (event) {
+let map;
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:8080/rest/schedules/get/for/" + document.getElementById("patient-id").value, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+        .then((response) => {
+            if (response.ok) {
+                response.json().then((responseJson) => {
+                    map = new Map(Object.entries(responseJson));
+                });
+            } else {
+                console.error("На сервере произошла ошибка, для которой здесь не предусмотрено никаких действий");
+            }
+        })
+        .catch((error) => {
+            console.error("Произошла ошибка, для которой не предусмотрено никаких действий", error);
+        });
+});
+
+document.querySelectorAll("a[data-key]").forEach((action) => {
+    action.addEventListener("click", function (event) {
         event.preventDefault();
 
-        let data = {
+        /*let data = {
             patientId: document.getElementById("patient-id").value,
             indicatorType: extractIndicatorType(edit.id),
-        };
+        };*/
 
-        fetchCheck(data);
+        let data;
+
+        schedule = map.get(action.dataset.key);
+
+        if (schedule.id == null) {
+            document.getElementById("delete").classList.add("visually-hidden");
+            document.getElementById("save").addEventListener("click", (event) => {
+                event.preventDefault();
+                data = null;
+                fetchAdd(data);
+            });
+        } else {
+            document.getElementById("delete").addEventListener("click", (event) => {
+                event.preventDefault();
+                id = null;
+                fetchDelete(id);
+            });
+            document.getElementById("save").addEventListener("click", (event) => {
+                event.preventDefault();
+                data = null;
+                fetchEdit(data);
+            });
+        }
+
+        //fetchCheck(data);
     });
 });
 
-function fetchCheck(data) {
+function fetchAdd(data) {}
+
+function fetchEdit(data) {}
+
+function fetchDelete(id) {}
+
+/*function fetchCheck(data) {
     fetch("http://localhost:8080/rest/schedules/check", {
         method: "POST",
         headers: {
@@ -31,7 +84,7 @@ function fetchCheck(data) {
         .catch((error) => {
             console.error("Произошла ошибка, для которой не предусмотрено никаких действий", error);
         });
-}
+}*/
 
 function fillAndShowScheduleEditingModal(responseJson) {
     let indicator = document.getElementById("indicator");
