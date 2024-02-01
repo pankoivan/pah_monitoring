dd86db2f9c4d36d78887215229a46c1b8f78a05f
pankoivan <panivan98@yandex.ru>
2024-02-01 17:55:22 +0300

    Сверстал черновики окон редактирования информации о пользователе, а также черновики окон по работе с активностью пользователя.

--- src/main/resources/static/js/users/user.js
+++ src/main/resources/static/js/users/user.js
@@ -2,3 +2,11 @@ document.getElementById("doctor-editing").addEventListener("click", function (ev
     event.preventDefault();
     new bootstrap.Modal(document.getElementById("doctor-editing-modal")).show();
 });
+
+document.getElementById("change-password").addEventListener("change", (check) => {
+    if (check.target.checked) {
+        document.getElementById("password-block").classList.remove("visually-hidden");
+    } else {
+        document.getElementById("password-block").classList.add("visually-hidden");
+    }
+});
