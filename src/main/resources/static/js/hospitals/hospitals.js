87d0018528f633d205ecb834bbc8bc5ccd62b697
pankoivan <panivan98@yandex.ru>
2024-02-03 16:21:26 +0300

    Реализовал все необходимые функции для работы с URL для будущей сортировки в js-файле.

--- src/main/resources/static/js/hospitals/hospitals.js
+++ src/main/resources/static/js/hospitals/hospitals.js
@@ -1,17 +1,25 @@
-document.getElementById("filter-button").addEventListener("click", () => {
-    let filtration = console.log(new URLSearchParams(window.location.search).get("byb"));
-});
-
 document.addEventListener("DOMContentLoaded", () => {
     const params = new URLSearchParams(window.location.search);
-    let filtration = params.get("filtration");
-    let sorting = params.get("sorting");
-    let searching = params.get("searching");
-
-    checked(filtration);
-    checked(sorting);
-    searched(searching);
+    checked(params.get("filtration"));
+    checked(params.get("sorting"));
+    searched(params.get("searching"));
+    showFilterOnStart();
 });
+
+document.getElementById("filter-apply-button").addEventListener("click", () => {
+    applyFilter();
+});
+
+document.getElementById("filter-clear-button").addEventListener("click", () => {
+    clearFilter();
+});
+
+function showFilterOnStart() {
+    const params = new URLSearchParams(window.location.search);
+    if (params.size > 1 || (params.size == 1 && !params.get("page"))) {
+        document.getElementById("filter").classList.add("show");
+    }
+}
 
 function checked(id) {
     if (document.getElementById(id)) {
@@ -23,4 +31,31 @@ function checked(id) {
 function searched(searching) {
     document.querySelector('input[name="searching"]').value = searching;
     document.querySelector('input[name="searching"]').setAttribute("value", searching);
+}
+
+function applyFilter() {
+    const url = new URL(window.location.href);
+    const params = new URLSearchParams(window.location.search);
+    if (params.get("page")) {
+        url.searchParams.set("page", params.get("page"));
+    }
+    if (document.querySelector('input[name="filtration"]:checked')) {
+        url.searchParams.set("filtration", document.querySelector('input[name="filtration"]:checked').id);
+    }
+    if (document.querySelector('input[name="sorting"]:checked')) {
+        url.searchParams.set("sorting", document.querySelector('input[name="sorting"]:checked').id);
+    }
+    if (document.querySelector('input[name="searching"]').value != "") {
+        url.searchParams.set("searching", document.querySelector('input[name="searching"]').value);
+    }
+    window.location.href = url;
+}
+
+function clearFilter() {
+    const params = new URLSearchParams(window.location.search);
+    let url = window.location.href.split("?")[0];
+    if (params.get("page")) {
+        url = url + `?page=${params.get("page")}`;
+    }
+    window.location.href = url;
 }
