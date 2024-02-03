3ec71f4623f194bde1af8f4e1ce35838ef5cf745
pankoivan <panivan98@yandex.ru>
2024-02-03 15:24:25 +0300

    Дополнил js-код работой с get-параметрами в черновом виде.

--- src/main/resources/static/js/hospitals/hospitals.js
+++ src/main/resources/static/js/hospitals/hospitals.js
@@ -1,3 +1,26 @@
 document.getElementById("filter-button").addEventListener("click", () => {
-    console.log(new URLSearchParams(window.location.search).get("byb"));
+    let filtration = console.log(new URLSearchParams(window.location.search).get("byb"));
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const params = new URLSearchParams(window.location.search);
+    let filtration = params.get("filtration");
+    let sorting = params.get("sorting");
+    let searching = params.get("searching");
+
+    checked(filtration);
+    checked(sorting);
+    searched(searching);
+});
+
+function checked(id) {
+    if (document.getElementById(id)) {
+        document.getElementById(id).setAttribute("checked", "checked");
+        document.getElementById(id).checked = "checked";
+    }
+}
+
+function searched(searching) {
+    document.querySelector('input[name="searching"]').value = searching;
+    document.querySelector('input[name="searching"]').setAttribute("value", searching);
+}
