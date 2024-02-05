65f594021f4ea27644382d1a65cebdd86d5cd479
pankoivan <panivan98@yandex.ru>
2024-02-05 13:01:28 +0300

    Создал REST-контроллер для добавления всех возможных неактивностей пользователей.

---
+++ src/main/java/org/pah_monitoring/main/controllers/rest/users/InactivityRestController.java
@@ -0,0 +1,129 @@
+package org.pah_monitoring.main.controllers.rest.users;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.pah_monitoring.main.dto.in.users.inactivity.DismissalAddingDto;
+import org.pah_monitoring.main.dto.in.users.inactivity.PatientInactivityAddingDto;
+import org.pah_monitoring.main.dto.in.users.inactivity.SickLeaveAddingDto;
+import org.pah_monitoring.main.dto.in.users.inactivity.VacationAddingDto;
+import org.pah_monitoring.main.dto.in.users.users.adding.PatientAddingDto;
+import org.pah_monitoring.main.dto.in.users.users.editing.PatientEditingDto;
+import org.pah_monitoring.main.dto.in.users.users.saving.PatientSavingDto;
+import org.pah_monitoring.main.dto.out.users.inactivity.common.InactivityOutDto;
+import org.pah_monitoring.main.entities.main.users.inactivity.Dismissal;
+import org.pah_monitoring.main.entities.main.users.inactivity.PatientInactivity;
+import org.pah_monitoring.main.entities.main.users.inactivity.SickLeave;
+import org.pah_monitoring.main.entities.main.users.inactivity.Vacation;
+import org.pah_monitoring.main.entities.main.users.inactivity.common.Inactivity;
+import org.pah_monitoring.main.entities.main.users.users.Patient;
+import org.pah_monitoring.main.entities.main.users.users.common.interfaces.HospitalEmployee;
+import org.pah_monitoring.main.exceptions.controller.rest.bad_request.DataValidationRestControllerException;
+import org.pah_monitoring.main.exceptions.controller.rest.forbidden.NotEnoughRightsRestControllerException;
+import org.pah_monitoring.main.exceptions.controller.rest.internal_server.DataSavingRestControllerException;
+import org.pah_monitoring.main.exceptions.service.access.NotEnoughRightsServiceException;
+import org.pah_monitoring.main.exceptions.service.data.DataSavingServiceException;
+import org.pah_monitoring.main.exceptions.service.data.DataSearchingServiceException;
+import org.pah_monitoring.main.exceptions.service.data.DataValidationServiceException;
+import org.pah_monitoring.main.mappers.common.interfaces.BaseEntityToOutDtoMapper;
+import org.pah_monitoring.main.services.additional.users.interfaces.UserSearchingService;
+import org.pah_monitoring.main.services.main.users.inactivity.interfaces.common.InactivityService;
+import org.pah_monitoring.main.services.main.users.users.interfaces.common.HospitalUserService;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/rest/inactivity/add")
+@PreAuthorize("isAuthenticated()")
+public class InactivityRestController {
+
+    @Qualifier("vacationService")
+    private final InactivityService<Vacation, VacationAddingDto, HospitalEmployee> vacationService;
+
+    @Qualifier("sickLeaveService")
+    private final InactivityService<SickLeave, SickLeaveAddingDto, HospitalEmployee> sickLeaveService;
+
+    @Qualifier("dismissalService")
+    private final InactivityService<Dismissal, DismissalAddingDto, HospitalEmployee> dismissalService;
+
+    @Qualifier("patientInactivityService")
+    private final InactivityService<PatientInactivity, PatientInactivityAddingDto, Patient> patientInactivityService;
+
+    @Qualifier("inactivityMapper")
+    private final BaseEntityToOutDtoMapper<Inactivity, InactivityOutDto> inactivityMapper;
+
+    @Qualifier("patientService")
+    private final HospitalUserService<Patient, PatientAddingDto, PatientEditingDto, PatientSavingDto> patientService;
+
+    private final UserSearchingService searchingService;
+
+    @PostMapping("/vacation")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public InactivityOutDto addVacation(@RequestBody @Valid VacationAddingDto addingDto, BindingResult bindingResult) {
+        try {
+            vacationService.checkAccessRightsForAdding(searchingService.findHospitalEmployeeByEmployeeInformationId(addingDto.getToWhomId()));
+            vacationService.checkDataValidityForAdding(addingDto, bindingResult);
+            return inactivityMapper.map(vacationService.add(addingDto));
+        } catch (DataSearchingServiceException | DataValidationServiceException e) {
+            throw new DataValidationRestControllerException(e.getMessage(), e);
+        } catch (NotEnoughRightsServiceException e) {
+            throw new NotEnoughRightsRestControllerException(e.getMessage(), e);
+        } catch (DataSavingServiceException e) {
+            throw new DataSavingRestControllerException(e.getMessage(), e);
+        }
+    }
+
+    @PostMapping("/sick-leave")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public InactivityOutDto addSickLeave(@RequestBody @Valid SickLeaveAddingDto addingDto, BindingResult bindingResult) {
+        try {
+            sickLeaveService.checkAccessRightsForAdding(searchingService.findHospitalEmployeeByEmployeeInformationId(addingDto.getToWhomId()));
+            sickLeaveService.checkDataValidityForAdding(addingDto, bindingResult);
+            return inactivityMapper.map(sickLeaveService.add(addingDto));
+        } catch (DataSearchingServiceException | DataValidationServiceException e) {
+            throw new DataValidationRestControllerException(e.getMessage(), e);
+        } catch (NotEnoughRightsServiceException e) {
+            throw new NotEnoughRightsRestControllerException(e.getMessage(), e);
+        } catch (DataSavingServiceException e) {
+            throw new DataSavingRestControllerException(e.getMessage(), e);
+        }
+    }
+
+    @PostMapping("/dismissal")
+    @PreAuthorize("hasRole('ADMINISTRATOR')")
+    public InactivityOutDto addDismissal(@RequestBody @Valid DismissalAddingDto addingDto, BindingResult bindingResult) {
+        try {
+            dismissalService.checkAccessRightsForAdding(searchingService.findHospitalEmployeeByEmployeeInformationId(addingDto.getToWhomId()));
+            dismissalService.checkDataValidityForAdding(addingDto, bindingResult);
+            return inactivityMapper.map(dismissalService.add(addingDto));
+        } catch (DataSearchingServiceException | DataValidationServiceException e) {
+            throw new DataValidationRestControllerException(e.getMessage(), e);
+        } catch (NotEnoughRightsServiceException e) {
+            throw new NotEnoughRightsRestControllerException(e.getMessage(), e);
+        } catch (DataSavingServiceException e) {
+            throw new DataSavingRestControllerException(e.getMessage(), e);
+        }
+    }
+
+    @PostMapping("/patient-inactivity")
+    @PreAuthorize("hasRole('DOCTOR')")
+    public InactivityOutDto addPatientInactivity(@RequestBody @Valid PatientInactivityAddingDto addingDto, BindingResult bindingResult) {
+        try {
+            patientInactivityService.checkAccessRightsForAdding(patientService.findById(addingDto.getToWhomId()));
+            patientInactivityService.checkDataValidityForAdding(addingDto, bindingResult);
+            return inactivityMapper.map(patientInactivityService.add(addingDto));
+        } catch (DataSearchingServiceException | DataValidationServiceException e) {
+            throw new DataValidationRestControllerException(e.getMessage(), e);
+        } catch (NotEnoughRightsServiceException e) {
+            throw new NotEnoughRightsRestControllerException(e.getMessage(), e);
+        } catch (DataSavingServiceException e) {
+            throw new DataSavingRestControllerException(e.getMessage(), e);
+        }
+    }
+
+}
