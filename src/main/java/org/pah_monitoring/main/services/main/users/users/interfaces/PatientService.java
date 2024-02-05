330081a6a568ebd126867513bd3b0aac2abe4e1f
pankoivan <panivan98@yandex.ru>
2024-02-05 18:39:45 +0300

    Убрал лишнее исключение в сервисе пациента.

--- src/main/java/org/pah_monitoring/main/services/main/users/users/interfaces/PatientService.java
+++ src/main/java/org/pah_monitoring/main/services/main/users/users/interfaces/PatientService.java
@@ -7,7 +7,6 @@ import org.pah_monitoring.main.entities.main.enums.AchievementEnum;
 import org.pah_monitoring.main.entities.main.users.users.Doctor;
 import org.pah_monitoring.main.entities.main.users.users.Patient;
 import org.pah_monitoring.main.exceptions.service.access.NotEnoughRightsServiceException;
-import org.pah_monitoring.main.exceptions.service.data.DataSavingServiceException;
 import org.pah_monitoring.main.exceptions.service.data.DataSearchingServiceException;
 import org.pah_monitoring.main.filtration.filters.common.EntityFilter;
 import org.pah_monitoring.main.services.main.users.users.interfaces.common.HospitalUserService;
@@ -17,7 +16,7 @@ import java.util.Map;
 
 public interface PatientService extends HospitalUserService<Patient, PatientAddingDto, PatientEditingDto, PatientSavingDto> {
 
-    void award(Patient patient, AchievementEnum achievement) throws DataSearchingServiceException, DataSavingServiceException;
+    void award(Patient patient, AchievementEnum achievement) throws DataSearchingServiceException;
 
     void assignToDoctor(Patient patient, Doctor doctor);
 
