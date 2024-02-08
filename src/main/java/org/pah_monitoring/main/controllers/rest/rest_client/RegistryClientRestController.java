cdae2c44954a7fe64b7c5f5026abf6714afaa60c
pankoivan <panivan98@yandex.ru>
2024-02-09 00:57:55 +0300

    Убрал лишний отладочный вывод на экран.

--- src/main/java/org/pah_monitoring/main/controllers/rest/rest_client/RegistryClientRestController.java
+++ src/main/java/org/pah_monitoring/main/controllers/rest/rest_client/RegistryClientRestController.java
@@ -24,7 +24,6 @@ public class RegistryClientRestController {
     @PostMapping("/search")
     public List<RegistryHospital> search(@RequestBody(required = false) String search) {
         try {
-            System.out.println("Controller: " + search);
             return service.search(search);
         } catch (RestClientServiceException e) {
             throw new RestClientRestControllerException(e.getMessage(), e);
