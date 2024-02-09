09cc5b75d328dd56abc1ddd48f3fdceb01e66305
pankoivan <panivan98@yandex.ru>
2024-02-09 14:34:19 +0300

    Убрал неиспользуемые импорты в out-DTO заявки на регистрацию больницы.

--- src/main/java/org/pah_monitoring/main/dto/out/hospitals/HospitalRegistrationRequestOutDto.java
+++ src/main/java/org/pah_monitoring/main/dto/out/hospitals/HospitalRegistrationRequestOutDto.java
@@ -2,10 +2,7 @@ package org.pah_monitoring.main.dto.out.hospitals;
 
 import lombok.Builder;
 import lombok.Data;
-import org.pah_monitoring.auxiliary.constants.DateTimeFormatConstants;
 import org.pah_monitoring.main.dto.out.common.interfaces.OutDto;
-
-import java.time.LocalDateTime;
 
 @Data
 @Builder
