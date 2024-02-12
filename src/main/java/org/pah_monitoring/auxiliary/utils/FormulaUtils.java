c52b7cc84964cd6da212aa7bc901dd73a290970a
pankoivan <panivan98@yandex.ru>
2024-02-12 12:44:09 +0300

    Добавил класс с формулами для будущего использования. Сразу добавил туда формулу для расчёта ИМТ.

---
+++ src/main/java/org/pah_monitoring/auxiliary/utils/FormulaUtils.java
@@ -0,0 +1,12 @@
+package org.pah_monitoring.auxiliary.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public final class FormulaUtils {
+
+    public static double bodyMassIndex(double weight, int height) {
+        return weight / (height * height);
+    }
+
+}
