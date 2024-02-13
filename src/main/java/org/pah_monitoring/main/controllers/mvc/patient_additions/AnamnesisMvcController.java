59fdb6719c82bb44fb1ebefa884d9aa7f132c352
pankoivan <panivan98@yandex.ru>
2024-02-13 13:52:16 +0300

    В MVC-контроллер добавил маппер для анамнеза, чтобы можно было видеть полный анамнез с учётом дополнительной информации (рассчитанный ИМТ).

--- src/main/java/org/pah_monitoring/main/controllers/mvc/patient_additions/AnamnesisMvcController.java
+++ src/main/java/org/pah_monitoring/main/controllers/mvc/patient_additions/AnamnesisMvcController.java
@@ -1,14 +1,17 @@
 package org.pah_monitoring.main.controllers.mvc.patient_additions;
 
 import lombok.AllArgsConstructor;
+import org.pah_monitoring.main.dto.out.patient_additions.AnamnesisOutDto;
 import org.pah_monitoring.main.entities.main.enums.TrueFalseEnum;
 import org.pah_monitoring.main.entities.main.patient_additions.Anamnesis;
 import org.pah_monitoring.main.exceptions.controller.mvc.UrlValidationMvcControllerException;
 import org.pah_monitoring.main.exceptions.service.data.DataSearchingServiceException;
 import org.pah_monitoring.main.exceptions.service.url.UrlValidationServiceException;
+import org.pah_monitoring.main.mappers.common.interfaces.BaseEntityToOutDtoMapper;
 import org.pah_monitoring.main.services.additional.mvc.interfaces.PageHeaderService;
 import org.pah_monitoring.main.services.additional.mvc.interfaces.RedirectService;
 import org.pah_monitoring.main.services.main.patient_additions.interfaces.AnamnesisService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,6 +31,9 @@ public class AnamnesisMvcController {
 
     private final PageHeaderService pageHeaderService;
 
+    @Qualifier("anamnesisMapper")
+    private final BaseEntityToOutDtoMapper<Anamnesis, AnamnesisOutDto> anamnesisMapper;
+
     @GetMapping("/form")
     @PreAuthorize("hasRole('PATIENT')")
     public String getAnamnesisFormPage(Model model) {
@@ -44,7 +50,7 @@ public class AnamnesisMvcController {
     @PreAuthorize("hasAnyRole('PATIENT', 'DOCTOR')")
     public String getAnamnesisPage(Model model, @PathVariable("patientId") String pathPatientId) {
         try {
-            model.addAttribute("anamnesis", service.findByPatientId(service.parsePathId(pathPatientId)));
+            model.addAttribute("anamnesis", anamnesisMapper.map(service.findByPatientId(service.parsePathId(pathPatientId))));
             pageHeaderService.addHeader(model);
             return "patients/anamnesis";
         } catch (UrlValidationServiceException | DataSearchingServiceException e) {
