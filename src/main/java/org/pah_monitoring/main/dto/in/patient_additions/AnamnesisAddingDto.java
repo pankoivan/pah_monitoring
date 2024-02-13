22988310167c0eab79d2a7b0d6020ea45c619428
pankoivan <panivan98@yandex.ru>
2024-02-13 18:13:51 +0300

    Поправил валидацию данных при добавке анамнеза: сделал сообщения об ошибках понятными для пациента.

--- src/main/java/org/pah_monitoring/main/dto/in/patient_additions/AnamnesisAddingDto.java
+++ src/main/java/org/pah_monitoring/main/dto/in/patient_additions/AnamnesisAddingDto.java
@@ -9,27 +9,27 @@ import org.pah_monitoring.main.entities.main.patient_additions.Anamnesis;
 @Data
 public class AnamnesisAddingDto {
 
-    @NotNull(message = "Поле \"heartDisease\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Хронические заболевания сердца\" является обязательным")
     private Boolean heartDisease;
 
-    @NotNull(message = "Поле \"lungDisease\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Хронические заболевания лёгких\" является обязательным")
     private Boolean lungDisease;
 
-    @NotNull(message = "Поле \"relativesDiseases\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Хронические заболевания сердца или лёгких у родственников\" является обязательным")
     private Boolean relativesDiseases;
 
-    @NotNull(message = "Поле \"bloodClotting\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Свёртываемость крови\" является обязательным")
     private Anamnesis.BloodClotting bloodClotting;
 
-    @NotNull(message = "Поле \"diabetes\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Сахарный диабет\" является обязательным")
     private Boolean diabetes;
 
-    @NotNull(message = "Поле \"height\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Рост\" является обязательным")
     @Min(value = 40, message = "Рост не может быть меньше 40 см")
     @Max(value = 220, message = "Рост не может быть больше 220 см")
     private Integer height;
 
-    @NotNull(message = "Поле \"weight\" не должно отсутствовать")
+    @NotNull(message = "Поле \"Вес\" является обязательным")
     @Min(value = 0, message = "Вес не может быть отрицательным")
     @Max(value = 250, message = "Вес не может превышать 250 кг")
     private Double weight;
