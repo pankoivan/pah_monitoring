b15841ea744b26ffa0d0e11415fa7ed9606ff7aa
pankoivan <panivan98@yandex.ru>
2024-02-17 17:03:49 +0300

    Поправил иерархию интерфейсов сущностей-показателей.

--- src/main/java/org/pah_monitoring/main/entities/main/examinations/indicators/common/interfaces/GraphicsTablesInputIndicator.java
+++ src/main/java/org/pah_monitoring/main/entities/main/examinations/indicators/common/interfaces/GraphicsTablesInputIndicator.java
@@ -1,5 +1,5 @@
 package org.pah_monitoring.main.entities.main.examinations.indicators.common.interfaces;
 
-public interface GraphicsTablesInputIndicator extends InputIndicator {
+public interface GraphicsTablesInputIndicator extends TablesInputIndicator {
 
 }
