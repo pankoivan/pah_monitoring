2bd4b483a32a658ff41e7c5c515b4116ac7e1524
pankoivan <panivan98@yandex.ru>
2024-02-18 00:11:30 +0300

    В соответствии с изменёнными сервисами изменил MVC-контроллер по выдаче карточек.

--- src/main/java/org/pah_monitoring/main/controllers/mvc/indicators/IndicatorMvcController.java
+++ src/main/java/org/pah_monitoring/main/controllers/mvc/indicators/IndicatorMvcController.java
@@ -1,6 +1,6 @@
 package org.pah_monitoring.main.controllers.mvc.indicators;
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.pah_monitoring.main.dto.in.users.users.patient.PatientAddingDto;
 import org.pah_monitoring.main.dto.in.users.users.patient.PatientEditingDto;
 import org.pah_monitoring.main.dto.in.users.users.patient.PatientSavingDto;
@@ -10,9 +10,9 @@ import org.pah_monitoring.main.exceptions.controller.mvc.UrlValidationMvcControl
 import org.pah_monitoring.main.exceptions.service.access.NotEnoughRightsServiceException;
 import org.pah_monitoring.main.exceptions.service.data.DataSearchingServiceException;
 import org.pah_monitoring.main.exceptions.service.url.UrlValidationServiceException;
+import org.pah_monitoring.main.services.additional.mvc.interfaces.PageHeaderService;
 import org.pah_monitoring.main.services.additional.users.interfaces.CurrentUserCheckService;
 import org.pah_monitoring.main.services.main.examinations.cards.interfaces.IndicatorCardService;
-import org.pah_monitoring.main.services.additional.mvc.interfaces.PageHeaderService;
 import org.pah_monitoring.main.services.main.users.users.interfaces.common.HospitalUserService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Controller
 @RequestMapping("/patients/{id}/indicators")
 @PreAuthorize("hasAnyRole('DOCTOR', 'PATIENT')")
@@ -42,8 +42,7 @@ public class IndicatorMvcController {
         try {
             Patient patient = patientService.findById(patientService.parsePathId(pathId));
             service.checkAccessRightsForObtainingAll(patient);
-            model.addAttribute("inputIndicatorCards", service.getAllInputIndicatorCardsFor(patient));
-            model.addAttribute("fileIndicatorCards", service.getAllFileIndicatorCardsFor(patient));
+            model.addAttribute("cards", service.getAllIndicatorCardsFor(patient));
             model.addAttribute("isCurrentUserOwnDoctor", checkService.isOwnDoctor(patient));
             model.addAttribute("isCurrentUserPatient", checkService.isSelf(patient));
             model.addAttribute("patient", patient);
