42712b075fa275c8b031a15e095f2cf19e4fcff4
pankoivan <panivan98@yandex.ru>
2024-02-18 15:32:59 +0300

    Добавил js-код для работы с формой отправки давления, поправил страницу формы давления.

---
+++ src/main/resources/static/js/indicators/forms/pressure-form.js
@@ -0,0 +1,49 @@
+const pressureForm = document.getElementById("pressure-form");
+
+pressureForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const data = {
+        upper: pressureForm.querySelector('input[name="upper"]').value,
+        lower: pressureForm.querySelector('input[name="lower"]').value,
+        afterExercise: checked(pressureForm.querySelector('input[name="afterExercise"]:checked')),
+    };
+
+    fetchAdd(data);
+});
+
+function fetchAdd(data) {
+    fetch("http://localhost:8080/rest/indicators/add/pressure", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => {
+            if (response.ok) {
+                showSuccessModal(response);
+            } else {
+                showErrorModal(response);
+            }
+        })
+        .catch((error) => {
+            console.error("Ошибка запроса", error);
+        });
+}
+
+function showSuccessModal() {
+    document.getElementById("success-modal-text").innerText = 'Показатель "Давление" был успешно отправлен';
+    new bootstrap.Modal(document.getElementById("success-modal")).show();
+}
+
+function showErrorModal(response) {
+    response.json().then((responseJson) => {
+        document.getElementById("error-modal-text").innerText = responseJson.errorDescription;
+        new bootstrap.Modal(document.getElementById("error-modal")).show();
+    });
+}
+
+function checked(checked) {
+    return checked ? checked.value : checked;
+}
