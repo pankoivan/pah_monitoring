124d52634af872720b63be8aceeda1f1e74371ba
pankoivan <panivan98@yandex.ru>
2024-02-18 13:48:36 +0300

    В черновом варианте добавил атрибуты-перечисления для выдачи формам отправки показателей.

--- src/main/java/org/pah_monitoring/main/controllers/mvc/indicators/IndicatorMvcController.java
+++ src/main/java/org/pah_monitoring/main/controllers/mvc/indicators/IndicatorMvcController.java
@@ -1,6 +1,9 @@
 package org.pah_monitoring.main.controllers.mvc.indicators;
 
 import lombok.AllArgsConstructor;
+import org.pah_monitoring.main.entities.main.enums.EventDuration;
+import org.pah_monitoring.main.entities.main.enums.TrueFalseEnum;
+import org.pah_monitoring.main.entities.main.examinations.indicators.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,46 +23,69 @@ public class IndicatorMvcController {
 
     @GetMapping("/walk-test")
     public String walkTest(Model model) {
+        model.addAttribute("oxygenSupport", TrueFalseEnum.values());
+        model.addAttribute("auxiliaryDevices", TrueFalseEnum.values());
+        model.addAttribute("breathlessness", WalkTest.Breathlessness.values());
         return "walk-test";
     }
 
     @GetMapping("/pulse-oximetry")
     public String pulseOximetry(Model model) {
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
         return "pulse-oximetry";
     }
 
     @GetMapping("/cough")
     public String cough(Model model) {
+        model.addAttribute("type", Cough.Type.values());
+        model.addAttribute("power", Cough.Power.values());
+        model.addAttribute("timbre", Cough.Timbre.values());
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
         return "cough";
     }
 
     @GetMapping("/chest-pain")
     public String chestPain(Model model) {
+        model.addAttribute("type", ChestPain.Type.values());
+        model.addAttribute("duration", EventDuration.values());
+        model.addAttribute("nitroglycerin", ChestPain.Nitroglycerin.values());
         return "chest-pain";
     }
 
     @GetMapping("/fainting")
     public String fainting(Model model) {
+        model.addAttribute("duration", EventDuration.values());
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
         return "fainting";
     }
 
     @GetMapping("/physical-changes")
     public String physicalChanges(Model model) {
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
+        model.addAttribute("legsSwelling", PhysicalChanges.LegsSwelling.values());
+        model.addAttribute("skinColor", PhysicalChanges.SkinColor.values());
         return "physical-changes";
     }
 
     @GetMapping("/overall-health")
     public String overallHealth(Model model) {
+        model.addAttribute("conditions", TrueFalseEnum.values());
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
+        model.addAttribute("weakness", OverallHealth.Weakness.values());
+        model.addAttribute("coldExtremities", OverallHealth.ColdExtremities.values());
         return "overall-health";
     }
 
     @GetMapping("/vertigo")
     public String vertigo(Model model) {
+        model.addAttribute("duration", EventDuration.values());
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
         return "vertigo";
     }
 
     @GetMapping("/pressure")
     public String pressure(Model model) {
+        model.addAttribute("trueFalse", TrueFalseEnum.values());
         return "pressure";
     }
 
