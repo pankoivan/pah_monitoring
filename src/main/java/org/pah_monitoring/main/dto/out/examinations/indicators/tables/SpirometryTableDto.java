877c538b9e5610562f90cefe73df14767c20c35c
pankoivan <panivan98@yandex.ru>
2024-02-19 20:24:48 +0300

    Добавил расчётные поля в table-DTO спирометрии.

--- src/main/java/org/pah_monitoring/main/dto/out/examinations/indicators/tables/SpirometryTableDto.java
+++ src/main/java/org/pah_monitoring/main/dto/out/examinations/indicators/tables/SpirometryTableDto.java
@@ -1,6 +1,5 @@
 package org.pah_monitoring.main.dto.out.examinations.indicators.tables;
 
-import jakarta.persistence.Column;
 import lombok.Builder;
 import lombok.Data;
 
@@ -17,5 +16,9 @@ public class SpirometryTableDto {
     private String rlv;
 
     private String vfe1;
+
+    private String tlc;
+
+    private String tiffnoIndex;
 
 }
