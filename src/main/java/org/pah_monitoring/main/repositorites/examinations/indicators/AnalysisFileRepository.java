7798b20ecac3690ae9989a42dd7e3dcb92802ef9
pankoivan <panivan98@yandex.ru>
2024-02-19 18:12:11 +0300

    Поправил обобщённый репозиторий показателей в соответствии с упрощённой иерархией интерфейсов основных сущностей показателей.

--- src/main/java/org/pah_monitoring/main/repositorites/examinations/indicators/AnalysisFileRepository.java
+++ src/main/java/org/pah_monitoring/main/repositorites/examinations/indicators/AnalysisFileRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 @Repository
 public interface AnalysisFileRepository extends JpaRepository<AnalysisFile, Integer> {
 
-    List<AnalysisFile> findAllByAnalysisTypeAndPatientId(AnalysisFile.AnalysisType type, Integer id);
+    List<AnalysisFile> findAllByAnalysisTypeAndPatientId(AnalysisFile.AnalysisType analysisType, Integer patientId);
 
 }
--- src/main/java/org/pah_monitoring/main/repositorites/examinations/indicators/common/InputIndicatorRepository.java
+++ src/main/java/org/pah_monitoring/main/repositorites/examinations/indicators/common/InputIndicatorRepository.java
@@ -1,13 +1,14 @@
 package org.pah_monitoring.main.repositorites.examinations.indicators.common;
 
+import org.pah_monitoring.main.entities.main.examinations.indicators.common.interfaces.Indicator;
 import org.pah_monitoring.main.entities.main.users.users.Patient;
 
 import java.util.List;
 
 public interface InputIndicatorRepository<T> {
 
-    List<T> findAllByPatientId(Integer id);
+    List<T> findAllByPatientId(Integer patientId);
 
-    List<InputIndicator> findAllByPatient(Patient patient);
+    List<Indicator> findAllByPatient(Patient patient);
 
 }
