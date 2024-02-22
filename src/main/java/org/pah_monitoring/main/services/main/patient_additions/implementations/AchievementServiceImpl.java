13270829f07bfbe3420af78a02bdc172aeb5e787
pankoivan <panivan98@yandex.ru>
2024-02-23 01:58:04 +0300

    Добавил использование рассылки при награждении пациента.

--- src/main/java/org/pah_monitoring/main/services/main/patient_additions/implementations/AchievementServiceImpl.java
+++ src/main/java/org/pah_monitoring/main/services/main/patient_additions/implementations/AchievementServiceImpl.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.pah_monitoring.main.email.interfaces.EmailSender;
 import org.pah_monitoring.main.entities.main.examinations.indicators.AnalysisFile;
 import org.pah_monitoring.main.entities.main.patient_additions.Achievement;
 import org.pah_monitoring.main.entities.main.users.users.Patient;
@@ -14,6 +15,7 @@ import org.pah_monitoring.main.services.additional.users.interfaces.CurrentUserC
 import org.pah_monitoring.main.services.main.patient_additions.interfaces.AchievementService;
 import org.pah_monitoring.main.services.main.users.users.interfaces.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -35,6 +37,9 @@ public class AchievementServiceImpl implements AchievementService {
     private PatientService patientService;
 
     private CurrentUserCheckService checkService;
+
+    @Qualifier("achievementEmailSender")
+    private EmailSender<Achievement> achievementEmailSender;
 
     @PostConstruct
     private void achievementsInit() {
@@ -86,102 +91,134 @@ public class AchievementServiceImpl implements AchievementService {
     }
 
     private void historian(Patient patient) {
-        if (patient.hasAnamnesis() && patient.hasNoAchievement(achievements.get("Историк"))) {
-            patientService.award(patient, achievements.get("Историк"));
+        Achievement achievement = achievements.get("Историк");
+        if (patient.hasAnamnesis() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void inhaleExhale(Patient patient) {
-        if (!patient.getSpirometries().isEmpty() && patient.hasNoAchievement(achievements.get("Вдох-выдох"))) {
-            patientService.award(patient, achievements.get("Вдох-выдох"));
+        Achievement achievement = achievements.get("Вдох-выдох");
+        if (!patient.getSpirometries().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void walk(Patient patient) {
-        if (!patient.getWalkTests().isEmpty() && patient.hasNoAchievement(achievements.get("Прогулка"))) {
-            patientService.award(patient, achievements.get("Прогулка"));
+        Achievement achievement = achievements.get("Прогулка");
+        if (!patient.getWalkTests().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void unconscious(Patient patient) {
-        if (!patient.getFaintings().isEmpty() && patient.hasNoAchievement(achievements.get("Без сознания"))) {
-            patientService.award(patient, achievements.get("Без сознания"));
+        Achievement achievement = achievements.get("Без сознания");
+        if (!patient.getFaintings().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void transformation(Patient patient) {
-        if (!patient.getPhysicalChanges().isEmpty() && patient.hasNoAchievement(achievements.get("Трансформация"))) {
-            patientService.award(patient, achievements.get("Трансформация"));
+        Achievement achievement = achievements.get("Трансформация");
+        if (!patient.getPhysicalChanges().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void athlete(Patient patient) {
-        if (!patient.getOverallHealths().isEmpty() && patient.hasNoAchievement(achievements.get("Спортсмен"))) {
-            patientService.award(patient, achievements.get("Спортсмен"));
+        Achievement achievement = achievements.get("Спортсмен");
+        if (!patient.getOverallHealths().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void waterBalance(Patient patient) {
-        if (!patient.getLiquids().isEmpty() && patient.hasNoAchievement(achievements.get("Водный баланс"))) {
-            patientService.award(patient, achievements.get("Водный баланс"));
+        Achievement achievement = achievements.get("Водный баланс");
+        if (!patient.getLiquids().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void diet(Patient patient) {
-        if (!patient.getWeights().isEmpty() && patient.hasNoAchievement(achievements.get("Диета"))) {
-            patientService.award(patient, achievements.get("Диета"));
+        Achievement achievement = achievements.get("Диета");
+        if (!patient.getWeights().isEmpty() && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void desperate(Patient patient) {
+        Achievement achievement = achievements.get("Отчаянный");
         int catheterizationCount = (int) patient.getAnalysisFiles()
                 .stream()
                 .filter(analysisFile -> analysisFile.getAnalysisType() == AnalysisFile.AnalysisType.CATHETERIZATION)
                 .count();
-        if (catheterizationCount >= 1 && patient.hasNoAchievement(achievements.get("Отчаянный"))) {
-            patientService.award(patient, achievements.get("Отчаянный"));
+        if (catheterizationCount >= 1 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void beginner(Patient patient) {
-        if (patient.getIndicatorsCount() >= 5 && patient.hasNoAchievement(achievements.get("Начинающий"))) {
-            patientService.award(patient, achievements.get("Начинающий"));
+        Achievement achievement = achievements.get("Начинающий");
+        if (patient.getIndicatorsCount() >= 5 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void verified(Patient patient) {
-        if (patient.getIndicatorsCount() >= 10 && patient.hasNoAchievement(achievements.get("Проверенный"))) {
-            patientService.award(patient, achievements.get("Проверенный"));
+        Achievement achievement = achievements.get("Проверенный");
+        if (patient.getIndicatorsCount() >= 10 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void diligent(Patient patient) {
-        if (patient.getIndicatorsCount() >= 20 && patient.hasNoAchievement(achievements.get("Старательный"))) {
-            patientService.award(patient, achievements.get("Старательный"));
+        Achievement achievement = achievements.get("Старательный");
+        if (patient.getIndicatorsCount() >= 20 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void persistent(Patient patient) {
-        if (patient.getIndicatorsCount() >= 50 && patient.hasNoAchievement(achievements.get("Упорный"))) {
-            patientService.award(patient, achievements.get("Упорный"));
+        Achievement achievement = achievements.get("Упорный");
+        if (patient.getIndicatorsCount() >= 50 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void purposeful(Patient patient) {
-        if (patient.getIndicatorsCount() >= 100 && patient.hasNoAchievement(achievements.get("Целеустремлённый"))) {
-            patientService.award(patient, achievements.get("Целеустремлённый"));
+        Achievement achievement = achievements.get("Целеустремлённый");
+        if (patient.getIndicatorsCount() >= 100 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void unshakable(Patient patient) {
-        if (patient.getIndicatorsCount() >= 250 && patient.hasNoAchievement(achievements.get("Непоколебимый"))) {
-            patientService.award(patient, achievements.get("Непоколебимый"));
+        Achievement achievement = achievements.get("Непоколебимый");
+        if (patient.getIndicatorsCount() >= 250 && patient.hasNoAchievement(achievement)) {
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
     private void champion(Patient patient) {
+        Achievement achievement = achievements.get("Чемпион");
         if (patient.getAchievementsCount() == achievements.size() - 1) {
-            patientService.award(patient, achievements.get("Чемпион"));
+            patientService.award(patient, achievement);
+            achievementEmailSender.send(patient.getUserSecurityInformation().getEmail(), achievement);
         }
     }
 
