1ece5b52c3369957b11c9681699ac384394d6c63
pankoivan <panivan98@yandex.ru>
2024-02-23 00:56:38 +0300

    Убрал тестовые строки в emailSender-е кодов.

--- src/main/java/org/pah_monitoring/main/email/implementations/RegistrationSecurityCodeEmailSenderImpl.java
+++ src/main/java/org/pah_monitoring/main/email/implementations/RegistrationSecurityCodeEmailSenderImpl.java
@@ -34,7 +34,7 @@ public class RegistrationSecurityCodeEmailSenderImpl implements EmailSender<Regi
     public void send(String recipient, RegistrationSecurityCode code) throws EmailSendingException {
         if (enabled) {
             try {
-                //allowedRecipients(recipient); // todo: delete in final version
+                allowedRecipients(recipient); // todo: delete in final version
                 MimeMessage message = mailSender.createMimeMessage();
                 MimeMessageHelper helper = new MimeMessageHelper(message);
 
@@ -50,18 +50,14 @@ public class RegistrationSecurityCodeEmailSenderImpl implements EmailSender<Regi
                         DateTimeFormatConstants.DAY_MONTH_YEAR_AT_HOUR_MINUTE_SECOND.format(code.getExpirationDate())
                 ), true);
 
-                throw new RuntimeException("Test");
-
-                //mailSender.send(message);
+                mailSender.send(message);
             } catch (Exception e) {
                 throw new EmailSendingException("Произошла ошибка при генерации кода. Повторите попытку позже", e);
             }
         }
     }
 
-    // todo: delete in final version
-
-    private void allowedRecipients(String to) {
+    private void allowedRecipients(String to) { // todo: delete in final version
         if (!List.of("pank-tanya@yandex.ru", "ivan.tornado2@yandex.ru").contains(to)) {
             throw new Error("Recipient \"%s\" is not allowed".formatted(to));
         }
