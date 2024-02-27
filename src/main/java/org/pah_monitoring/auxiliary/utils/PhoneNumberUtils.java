48feb556c06d1a97e9a92f1c3f86eeaff63631d7
pankoivan <panivan98@yandex.ru>
2024-02-27 19:18:17 +0300

    Поправил utility-класс по работе с номерами телефона, внедрил использование новой аннотации.

--- src/main/java/org/pah_monitoring/auxiliary/utils/PhoneNumberUtils.java
+++ src/main/java/org/pah_monitoring/auxiliary/utils/PhoneNumberUtils.java
@@ -1,11 +1,13 @@
 package org.pah_monitoring.auxiliary.utils;
 
 import lombok.experimental.UtilityClass;
+import org.pah_monitoring.main.aop.annotations.NullWhenNull;
 import org.pah_monitoring.main.exceptions.utils.PhoneNumberUtilsException;
 
 @UtilityClass
 public final class PhoneNumberUtils {
 
+    @NullWhenNull
     public static String toReadable(String sourcePhoneNumber) throws PhoneNumberUtilsException {
         if (!sourcePhoneNumber.matches("^\\d{11}$")) {
             throw new PhoneNumberUtilsException(
@@ -21,6 +23,7 @@ public final class PhoneNumberUtils {
         );
     }
 
+    @NullWhenNull
     public static String toSource(String readablePhoneNumber) {
         return readablePhoneNumber.replaceAll("[^0-9]", "");
     }
