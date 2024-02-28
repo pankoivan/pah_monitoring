a838a552880301b83d5a1edebb09b46383d53724
pankoivan <panivan98@yandex.ru>
2024-02-28 18:28:37 +0300

    Добавил пропущенные аннотации для внедрения зависимостей в walkTest-маппер графика.

--- src/main/java/org/pah_monitoring/main/mappers/examinations/indicators/graphics/WalkTestToGraphicDtoMapper.java
+++ src/main/java/org/pah_monitoring/main/mappers/examinations/indicators/graphics/WalkTestToGraphicDtoMapper.java
@@ -1,5 +1,7 @@
 package org.pah_monitoring.main.mappers.examinations.indicators.graphics;
 
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import org.pah_monitoring.auxiliary.utils.NumberUtils;
 import org.pah_monitoring.main.aop.annotations.NullWhenNull;
 import org.pah_monitoring.main.dto.out.examinations.indicators.graphics.PressureGraphicDto;
@@ -10,9 +12,12 @@ import org.pah_monitoring.main.entities.main.examinations.indicators.PulseOximet
 import org.pah_monitoring.main.entities.main.examinations.indicators.WalkTest;
 import org.pah_monitoring.main.mappers.common.interfaces.BaseEntityToOutDtoListMapper;
 import org.pah_monitoring.main.mappers.common.interfaces.BaseEntityToOutDtoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Setter(onMethod = @__(@Autowired))
 @Component("walkTestGraphicMapper")
 public class WalkTestToGraphicDtoMapper implements BaseEntityToOutDtoListMapper<WalkTest, WalkTestGraphicDto> {
 
