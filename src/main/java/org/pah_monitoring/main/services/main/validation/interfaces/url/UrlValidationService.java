e428942079a5f0d89b9ac69b5083fe441884fa57
pankoivan <panivan98@yandex.ru>
2024-02-29 18:48:58 +0300

    Поправил тексты ошибок в сервисе валидации url.

--- src/main/java/org/pah_monitoring/main/services/main/validation/interfaces/url/UrlValidationService.java
+++ src/main/java/org/pah_monitoring/main/services/main/validation/interfaces/url/UrlValidationService.java
@@ -4,15 +4,15 @@ import org.pah_monitoring.main.exceptions.service.url.UrlValidationServiceExcept
 
 public interface UrlValidationService {
 
-    default int parsePathId(String pathId) throws UrlValidationServiceException {
+    default Integer parsePathId(String pathId) throws UrlValidationServiceException {
         int id;
         try {
             id = Integer.parseInt(pathId);
         } catch (NumberFormatException e) {
-            throw new UrlValidationServiceException("Некорректный идентификатор: \"%s\"".formatted(pathId));
+            throw new UrlValidationServiceException("Некорректный id: \"%s\"".formatted(pathId));
         }
         if (id <= 0) {
-            throw new UrlValidationServiceException("Некорректный идентификатор: \"%s\"".formatted(pathId));
+            throw new UrlValidationServiceException("Некорректный id: \"%s\"".formatted(pathId));
         }
         return id;
     }
