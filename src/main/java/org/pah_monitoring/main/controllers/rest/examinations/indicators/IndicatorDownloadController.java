44d78c91a7ad58c56dfebf0413d0c4047bad82c9
pankoivan <panivan98@yandex.ru>
2024-03-03 16:27:27 +0300

    Исправил работу со скачиванием файлов: добавил возможность скачивания файлов с русскими символами в названии.

--- src/main/java/org/pah_monitoring/main/controllers/rest/examinations/indicators/IndicatorDownloadController.java
+++ src/main/java/org/pah_monitoring/main/controllers/rest/examinations/indicators/IndicatorDownloadController.java
@@ -11,7 +11,7 @@ import org.pah_monitoring.main.exceptions.service.data.DataSearchingServiceExcep
 import org.pah_monitoring.main.services.main.examinations.indicators.interfaces.common.FileIndicatorService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -34,13 +35,20 @@ public class IndicatorDownloadController {
     @Qualifier("analysisFileService")
     private final FileIndicatorService<AnalysisFile> analysisFileService;
 
-    @GetMapping(produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    public byte[] downloadAnalysisFile(@PathVariable("filename") String filename) {
+    @GetMapping
+    public ResponseEntity<byte[]> download(@PathVariable("filename") String filename) {
         try {
             AnalysisFile analysisFile = analysisFileService.findByFilename(filename);
             Patient patient = analysisFile.getPatient();
             analysisFileService.checkAccessRightsForObtaining(patient);
-            return Files.readAllBytes(Paths.get(uploadPath, filename));
+            //return Files.readAllBytes(Paths.get(uploadPath, filename));
+            HttpHeaders headers = new HttpHeaders();
+            MediaType mediaType = MediaType.APPLICATION_PDF;
+            //headers.add(HttpHeaders.CONTENT_DISPOSITION, "");
+            //headers.setContentType(new MediaType(MediaType.APPLICATION_PDF, StandardCharsets.UTF_8));
+            //headers.setContentDispositionFormData("attachment", filename);
+            headers.setContentDisposition(ContentDisposition.formData().filename(filename, StandardCharsets.UTF_8).build());
+            return new ResponseEntity<>(Files.readAllBytes(Paths.get(uploadPath, filename)), headers, HttpStatus.OK);
         } catch (DataSearchingServiceException e) {
             throw new UrlValidationRestControllerException(e.getMessage(), e);
         } catch (NotEnoughRightsServiceException e) {
