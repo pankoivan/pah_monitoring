d026c32983638251493733c550282e7e9a2b2490
pankoivan <panivan98@yandex.ru>
2024-03-03 21:27:35 +0300

    Поправил js-файл работы с периодами и страница с файлами анализов.

--- src/main/resources/static/js/selection/periodization.js
+++ src/main/resources/static/js/selection/periodization.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     showFilterOnStart();
 });
 
-document.getElementById("period-apply-button").addEventListener("click", () => {
+document.getElementById("filter-apply-button").addEventListener("click", () => {
     applyFilter();
 });
 
-document.getElementById("period-clear-button").addEventListener("click", () => {
+document.getElementById("filter-clear-button").addEventListener("click", () => {
     clearFilter();
 });
 
 function showFilterOnStart() {
     const params = new URLSearchParams(window.location.search);
-    if (params.get("filtration") || params.get("sorting") || params.get("searching")) {
+    if (params.get("period")) {
         document.getElementById("filter").classList.add("show");
     }
 }
@@ -26,33 +26,16 @@ function checked(id) {
     }
 }
 
-function searched(searching) {
-    document.querySelector('input[name="searching"]').value = searching;
-    document.querySelector('input[name="searching"]').setAttribute("value", searching);
-}
-
 function applyFilter() {
     const url = new URL(window.location.href);
-    if (document.querySelector('input[name="filtration"]:checked')) {
-        url.searchParams.set("filtration", document.querySelector('input[name="filtration"]:checked').id);
+    if (document.querySelector('input[name="period"]:checked')) {
+        url.searchParams.set("period", document.querySelector('input[name="period"]:checked').id);
     }
-    if (document.querySelector('input[name="sorting"]:checked')) {
-        url.searchParams.set("sorting", document.querySelector('input[name="sorting"]:checked').id);
-    }
-    if (document.querySelector('input[name="searching"]').value != "") {
-        url.searchParams.set("searching", document.querySelector('input[name="searching"]').value);
-    } else {
-        url.searchParams.delete("searching");
-    }
-    url.searchParams.set("page", 1);
     window.location.href = url;
 }
 
 function clearFilter() {
     const url = new URL(window.location.href);
-    url.searchParams.delete("filtration");
-    url.searchParams.delete("sorting");
-    url.searchParams.delete("searching");
-    url.searchParams.set("page", 1);
+    url.searchParams.delete("period");
     window.location.href = url;
 }
