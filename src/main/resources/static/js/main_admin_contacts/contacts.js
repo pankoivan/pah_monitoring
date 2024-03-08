7902f2739467a515e967038a18d6fd362c873f19
pankoivan <panivan98@yandex.ru>
2024-03-09 01:54:26 +0300

    Улучшил старую-добрую страницу контактов главного администратора. Добавил сообщение об успехе при сохранении и редактировании контакта. Изменил кнопки редактирования и удаления, изменил цвет фона формы в соответствии с остальными формами в приложении. Соответственно поправил js-код, улучшил его. Заменил svg "msgdelete".

--- src/main/resources/static/js/main_admin_contacts/contacts.js
+++ src/main/resources/static/js/main_admin_contacts/contacts.js
@@ -1,9 +1,9 @@
 const contactsForm = document.getElementById("contacts-form");
 
-contactsForm.addEventListener("submit", function (event) {
+contactsForm.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let data = {
+    const data = {
         id: contactsForm.querySelector('input[name="id"]').value,
         contact: contactsForm.querySelector('input[name="contact"]').value,
         description: contactsForm.querySelector('input[name="description"]').value,
@@ -17,16 +17,15 @@ Array.from(document.getElementById("contacts").children).forEach((contact) => {
 });
 
 function addEditDeleteEventListenersToContact(contact) {
-    let contactId = extractEntityId(contact.id);
+    const contactId = extractEntityId(contact.id);
 
-    document.getElementById("edit-contact-" + contactId).addEventListener("click", function (event) {
-        event.preventDefault();
+    document.getElementById(`edit-contact-${contactId}`).addEventListener("click", () => {
         contactsForm.querySelector('input[name="id"]').value = contactId;
-        contactsForm.querySelector('input[name="contact"]').value = document.getElementById("contact-contact-" + contactId).textContent;
-        contactsForm.querySelector('input[name="description"]').value = document.getElementById("description-contact-" + contactId).textContent;
+        contactsForm.querySelector('input[name="contact"]').value = document.getElementById(`contact-contact-${contactId}`).textContent;
+        contactsForm.querySelector('input[name="description"]').value = document.getElementById(`description-contact-${contactId}`).textContent;
     });
 
-    document.getElementById("delete-contact-" + contactId).addEventListener("click", function (event) {
+    document.getElementById(`delete-contact-${contactId}`).addEventListener("click", (event) => {
         event.preventDefault();
         fetchDelete(contact);
     });
@@ -37,6 +36,7 @@ function fetchSave(data) {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
+            Accept: "application/json",
         },
         body: JSON.stringify(data),
     })
@@ -45,19 +45,19 @@ function fetchSave(data) {
                 contactsForm.reset();
                 response.json().then((responseJson) => {
                     try {
-                        document.getElementById("contact-contact-" + responseJson.id).innerText = responseJson.contact;
-                        document.getElementById("description-contact-" + responseJson.id).innerText = responseJson.description;
-                        console.log("Контакт был успешно изменён");
+                        document.getElementById(`contact-contact-${responseJson.id}`).innerText = responseJson.contact;
+                        document.getElementById(`description-contact-${responseJson.id}`).innerText = responseJson.description;
+                        showSuccessModal("Контакт был успешно изменён");
                     } catch (error) {
                         if (document.getElementById("empty-contacts-message")) {
                             removeEmptyContactsMessage();
                         }
                         newContact(responseJson, document.getElementById("contacts").childElementCount == 0);
-                        console.log("Контакт был успешно добавлен");
+                        showSuccessModal("Контакт был успешно добавлен");
                     }
                 });
             } else {
-                showModalError(response);
+                showErrorModal(response);
             }
         })
         .catch((error) => {
@@ -66,7 +66,7 @@ function fetchSave(data) {
 }
 
 function fetchDelete(contact) {
-    fetch("http://localhost:8080/rest/contacts/delete/" + extractEntityId(contact.id), {
+    fetch(`http://localhost:8080/rest/contacts/delete/${extractEntityId(contact.id)}`, {
         method: "POST",
     })
         .then((response) => {
@@ -75,16 +75,15 @@ function fetchDelete(contact) {
                 if (document.getElementById("contacts").childElementCount == 0) {
                     addEmptyContactsMessage();
                 } else {
-                    let contacts = Array.from(document.getElementById("contacts").children);
-                    let lastContact = contacts[contacts.length - 1];
-                    let lastContactHrs = lastContact.querySelectorAll("hr");
+                    const contacts = Array.from(document.getElementById("contacts").children);
+                    const lastContact = contacts[contacts.length - 1];
+                    const lastContactHrs = lastContact.querySelectorAll("hr");
                     if (lastContactHrs.length == 1) {
                         lastContact.removeChild(lastContactHrs[0]);
                     }
                 }
-                console.log("Контакт был успешно удалён");
             } else {
-                showModalError(response);
+                showErrorModal(response);
             }
         })
         .catch((error) => {
@@ -92,7 +91,12 @@ function fetchDelete(contact) {
         });
 }
 
-function showModalError(response) {
+function showSuccessModal(message) {
+    document.getElementById("success-modal-text").innerText = message;
+    new bootstrap.Modal(document.getElementById("success-modal")).show();
+}
+
+function showErrorModal(response) {
     response.json().then((responseJson) => {
         document.getElementById("error-modal-text").innerText = responseJson.errorDescription;
         new bootstrap.Modal(document.getElementById("error-modal")).show();
@@ -100,57 +104,62 @@ function showModalError(response) {
 }
 
 function newContact(responseJson, isFirst) {
-    let contact = document.createElement("div");
-    contact.id = "contact-" + responseJson.id;
+    const contact = document.createElement("div");
+    contact.id = `contact-${responseJson.id}`;
     contact.className = "text-start mb-2";
 
-    let contactDescription = document.createElement("h4");
-    contactDescription.id = "description-contact-" + responseJson.id;
-    contactDescription.className = "text-secondary";
+    const contactDescription = document.createElement("span");
+    contactDescription.id = `description-contact-${responseJson.id}`;
     contactDescription.textContent = responseJson.description;
 
-    let contactContact = document.createElement("p");
-    contactContact.id = "contact-contact-" + responseJson.id;
+    const contactDescriptionBlock = document.createElement("h4");
+    contactDescriptionBlock.appendChild(contactDescription);
+    contactDescriptionBlock.appendChild(editDeleteLink("edit", responseJson.id));
+    contactDescriptionBlock.appendChild(editDeleteLink("delete", responseJson.id));
+
+    const contactContact = document.createElement("p");
+    contactContact.id = `contact-contact-${responseJson.id}`;
     contactContact.className = "text-dark text-break mb-2";
     contactContact.textContent = responseJson.contact;
 
-    let contactEdit = document.createElement("a");
-    contactEdit.id = "edit-contact-" + responseJson.id;
-    contactEdit.href = "#";
-    contactEdit.className = "text-secondary me-1";
-    contactEdit.textContent = "Изменить";
+    const contactHr = document.createElement("hr");
 
-    let contactDelete = document.createElement("a");
-    contactDelete.id = "delete-contact-" + responseJson.id;
-    contactDelete.href = "#";
-    contactDelete.className = "text-secondary ms-1";
-    contactDelete.textContent = "Удалить";
-
-    let contactHr = document.createElement("hr");
-
-    contact.appendChild(contactDescription);
+    contact.appendChild(contactDescriptionBlock);
     contact.appendChild(contactContact);
-    contact.appendChild(contactEdit);
-    contact.appendChild(contactDelete);
     if (!isFirst) {
         contact.appendChild(contactHr);
     }
 
-    let contacts = document.getElementById("contacts");
+    const contacts = document.getElementById("contacts");
     contacts.insertBefore(contact, contacts.firstChild);
 
     addEditDeleteEventListenersToContact(contact);
 }
 
-function addEmptyContactsMessage() {
-    let row = document.createElement("div");
-    row.id = "empty-contacts-message";
-    row.className = "row";
+function editDeleteLink(which, id) {
+    const img = document.createElement("img");
+    img.width = 24;
+    img.src = "/svg/msg" + which + ".svg";
 
-    let col = document.createElement("div");
+    const a = document.createElement("a");
+    a.id = which + "-contact-" + id;
+    a.href = "#";
+    a.className = "text-decoration-none";
+
+    a.appendChild(img);
+
+    return a;
+}
+
+function addEmptyContactsMessage() {
+    const row = document.createElement("div");
+    row.className = "row";
+    row.id = "empty-contacts-message";
+
+    const col = document.createElement("div");
     col.className = "col-12";
 
-    let message = document.createElement("div");
+    const message = document.createElement("div");
     message.className = "text-secondary text-center fst-italic fs-1";
     message.textContent = "Список контактов пуст";
 
@@ -164,6 +173,5 @@ function removeEmptyContactsMessage() {
 }
 
 function extractEntityId(elementId) {
-    let parts = elementId.split("-");
-    return parts[parts.length - 1];
+    return elementId.split("-").pop();
 }
